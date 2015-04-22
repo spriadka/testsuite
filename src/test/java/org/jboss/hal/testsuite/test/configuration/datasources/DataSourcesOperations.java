5d594d55e91cd6fb2b25a138af9f288febaabce0
Michal Krajcovic <mkrajcov@redhat.com>
2015-04-22 11:18:30 +0200

    Added Datasources test cases

---
+++ src/test/java/org/jboss/hal/testsuite/test/configuration/datasources/DataSourcesOperations.java
@@ -0,0 +1,90 @@
+package org.jboss.hal.testsuite.test.configuration.datasources;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jboss.hal.testsuite.cli.CliClient;
+import org.jboss.hal.testsuite.cli.CliConstants;
+import org.jboss.hal.testsuite.cli.CliUtils;
+import org.jboss.hal.testsuite.test.util.ConfigUtils;
+
+/**
+ * Created by mkrajcov on 4/13/15.
+ */
+public class DataSourcesOperations {
+
+    private CliClient client;
+
+    private String profile = "";
+
+    public DataSourcesOperations(CliClient client) {
+        this.client = client;
+        if (ConfigUtils.isDomain()) {
+            profile = " --profile=full";
+        }
+    }
+
+    public DataSourcesOperations(CliClient client, String profile) {
+        this(client);
+        this.profile = " --profile=" + profile;
+    }
+
+    public static String getDsAddress(String name) {
+        return CliConstants.DATASOURCES_ADDRESS + "=" + name;
+    }
+
+    public static String getXADsAddress(String name) {
+        return CliConstants.XA_DATASOURCES_ADDRESS + "=" + name;
+    }
+
+    public String createDataSource(String name, String url) {
+        String command = "data-source add" +
+                profile +
+                " --name=" + name +
+                " --jndi-name=java:/datasources/" + name +
+                " --driver-name=h2" +
+                " --connection-url=\"" + url + "\"" +
+                " --enabled=true";
+        client.executeCommand(command);
+        return name;
+    }
+
+    public String createDataSource(String url) {
+        String name = RandomStringUtils.randomAlphanumeric(5);
+        return createDataSource(name, url);
+    }
+
+    public String createXADataSource(String name, String url) {
+        String command = "xa-data-source add" +
+                profile +
+                " --name=" + name +
+                " --jndi-name=java:/xa-datasources/" + name +
+                " --driver-name=h2" +
+                " --enabled=true";
+        String command2 = CliUtils.buildCommand(getXADsAddress(name) + "/xa-datasource-properties=URL", ":add(value=\"" + url + "\")");
+        client.executeCommand(command);
+        client.executeCommand(command2);
+        return name;
+    }
+
+    public String createXADataSource(String url) {
+        String name = RandomStringUtils.randomAlphanumeric(5);
+        return createXADataSource(name, url);
+    }
+
+    public void removeXADataSource(String name) {
+        String command = "xa-data-source remove" +
+                profile +
+                " --name=" + name;
+        client.executeCommand(command);
+    }
+
+    public void removeDataSource(String name) {
+        String command = "data-source remove" +
+                profile +
+                " --name=" + name;
+        client.executeCommand(command);
+    }
+
+    public boolean exists(String address) {
+        return client.executeForSuccess(address + ":read-resource");
+    }
+}
