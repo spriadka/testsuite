b6402773b5ecd60a73fc1fc5ab55cc6173938e18
Michal Krajcovic <mkrajcov@redhat.com>
2015-05-07 10:59:26 +0200

    Added network interface test case

---
+++ src/test/java/org/jboss/hal/testsuite/test/configuration/general/NetworkInterfacesTestCase.java
@@ -0,0 +1,207 @@
+package org.jboss.hal.testsuite.test.configuration.general;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.hal.testsuite.cli.CliClient;
+import org.jboss.hal.testsuite.fragment.config.interfaces.NetworkInterfaceContentFragment;
+import org.jboss.hal.testsuite.fragment.config.interfaces.NetworkInterfaceWizard;
+import org.jboss.hal.testsuite.page.config.NetworkInterfacesPage;
+import org.jboss.hal.testsuite.test.category.Standalone;
+import org.jboss.hal.testsuite.util.Console;
+import org.jboss.hal.testsuite.util.ResourceVerifier;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.jboss.hal.testsuite.cli.CliConstants.INTERFACE_ADDRESS;
+import static org.junit.Assert.*;
+
+/**
+ * @author mkrajcov <mkrajcov@redhat.com>
+ */
+@RunWith(Arquillian.class)
+@Category(Standalone.class)
+public class NetworkInterfacesTestCase {
+
+    private static final String INET_ADDRESS = "127.0.0.1";
+    private static final String WILDCARD_ANY_ADDRESS = "Any Address";
+    private static final String WILDCARD_ANY_IPV4 = "Any IP4";
+    private static final String WILDCARD_ANY_IPV6 = "Any IP6";
+    private static final String INTERFACE_NAME = "in_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String INTERFACE_ANY_NAME = "ininet_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String INTERFACE_ANY_IP4_NAME = "inip4_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String INTERFACE_ANY_IP6_NAME = "inip6_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String INTERFACE_NO_ADDRESS_NAME = "inno_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String NEW_NIC = "nic_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String NEW_NIC_MATCH = "nic_match_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String NEW_LOOPBACK_ADDRESS = "loopback_address_" + RandomStringUtils.randomAlphanumeric(5);
+
+    private static final String DMR_INTERFACE = INTERFACE_ADDRESS + "=" + INTERFACE_NAME;
+    private static final String DMR_INTERFACE_ANY = INTERFACE_ADDRESS + "=" + INTERFACE_ANY_NAME;
+    private static final String DMR_INTERFACE_NO_ADDRESS = INTERFACE_ADDRESS + "=" + INTERFACE_NO_ADDRESS_NAME;
+    private static final String DMR_INTERFACE_ANY_IP4 = INTERFACE_ADDRESS + "=" + INTERFACE_ANY_IP4_NAME;
+    private static final String DMR_INTERFACE_ANY_IP6 = INTERFACE_ADDRESS + "=" + INTERFACE_ANY_IP6_NAME;
+
+    private static CliClient client = new CliClient();
+    private static ResourceVerifier verifier = new ResourceVerifier(DMR_INTERFACE,client);
+
+    @Drone
+    public WebDriver browser;
+
+    @Page
+    public NetworkInterfacesPage page;
+
+    @Before
+    public void before(){
+        browser.navigate().refresh();
+        Graphene.goTo(NetworkInterfacesPage.class);
+        Console.withBrowser(browser).waitUntilLoaded();
+        browser.manage().window().maximize();
+    }
+
+    @After
+    public void after(){
+        client.reload(false);
+    }
+
+    @AfterClass
+    public static void cleanUp(){
+        client.removeResource(DMR_INTERFACE);
+        client.removeResource(DMR_INTERFACE_ANY);
+        client.removeResource(DMR_INTERFACE_NO_ADDRESS);
+        client.removeResource(DMR_INTERFACE_ANY_IP4);
+        client.removeResource(DMR_INTERFACE_ANY_IP6);
+    }
+
+    @Test
+    @InSequence(0)
+    public void createInterfaceSpecificInetAddress(){
+        NetworkInterfaceContentFragment area = page.getContent();
+        NetworkInterfaceWizard wizard = area.addInterface();
+
+        boolean result = wizard
+                .name(INTERFACE_NAME)
+                .inetAddress(INET_ADDRESS)
+                .finish();
+
+        assertTrue("Window should be closed", result);
+        assertTrue("Interface should be present in table", area.resourceIsPresent(INTERFACE_NAME));
+        verifier.verifyResource(DMR_INTERFACE, true);
+    }
+
+
+
+    @Test
+    @InSequence(1)
+    public void changeNicAttribute() {
+        NetworkInterfaceContentFragment area = page.getContent();
+
+        area.editNicAndSave(INTERFACE_NAME, NEW_NIC);
+
+        verifier.verifyAttribute("nic", NEW_NIC);
+    }
+
+    @Test
+    @InSequence(2)
+    public void changeNicMatchAttribute(){
+        NetworkInterfaceContentFragment area = page.getContent();
+
+        area.editNicMatchAndSave(INTERFACE_NAME, NEW_NIC_MATCH);
+
+        verifier.verifyAttribute("nic-match", NEW_NIC_MATCH);
+    }
+
+    @Test
+    @InSequence(3)
+    public void enableLoopBackAddress(){
+        NetworkInterfaceContentFragment area = page.getContent();
+
+        area.editLoopbackAddressAndSave(INTERFACE_NAME, NEW_LOOPBACK_ADDRESS);
+
+        verifier.verifyAttribute("loopback-address", NEW_LOOPBACK_ADDRESS);
+    }
+
+    @Test
+    @InSequence(4)
+    public void removeInterface(){
+        NetworkInterfaceContentFragment area = page.getContent();
+
+        area.removeInterface(INTERFACE_NAME);
+        verifier.verifyResource(DMR_INTERFACE, false);
+    }
+
+    @Test
+    public void createInterfaceAnyInetAddress(){
+        NetworkInterfaceContentFragment area = page.getContent();
+        NetworkInterfaceWizard wizard = area.addInterface();
+
+        boolean result = wizard
+                .name(INTERFACE_ANY_NAME)
+                .addressWildcard(WILDCARD_ANY_ADDRESS)
+                .finish();
+
+        assertTrue("Window should be closed", result);
+        assertTrue("Interface should be present in table", area.resourceIsPresent(INTERFACE_ANY_NAME));
+        verifier.verifyResource(DMR_INTERFACE_ANY, true);
+
+        area.removeInterface(INTERFACE_ANY_NAME);
+        verifier.verifyResource(DMR_INTERFACE_ANY, false);
+    }
+
+    @Test
+    public void createInterfaceAnyIPv4Address(){
+        NetworkInterfaceContentFragment area = page.getContent();
+        NetworkInterfaceWizard wizard = area.addInterface();
+
+        boolean result = wizard
+                .name(INTERFACE_ANY_IP4_NAME)
+                .addressWildcard(WILDCARD_ANY_IPV4)
+                .finish();
+
+        assertTrue("Window should be closed", result);
+        assertTrue("Interface should be present in table", area.resourceIsPresent(INTERFACE_ANY_IP4_NAME));
+        verifier.verifyResource(DMR_INTERFACE_ANY_IP4, true);
+
+        area.removeInterface(INTERFACE_ANY_IP4_NAME);
+        verifier.verifyResource(DMR_INTERFACE_ANY_IP4, false);
+    }
+
+    @Test
+    public void createInterfaceAnyIPv6Address(){
+        NetworkInterfaceContentFragment area = page.getContent();
+        NetworkInterfaceWizard wizard = area.addInterface();
+
+        boolean result = wizard
+                .name(INTERFACE_ANY_IP6_NAME)
+                .addressWildcard(WILDCARD_ANY_IPV6)
+                .finish();
+
+        assertTrue("Window should be closed", result);
+        assertTrue("Interface should be present in table", area.resourceIsPresent(INTERFACE_ANY_IP6_NAME));
+        verifier.verifyResource(DMR_INTERFACE_ANY_IP6, true);
+
+        area.removeInterface(INTERFACE_ANY_IP6_NAME);
+        verifier.verifyResource(DMR_INTERFACE_ANY_IP6, false);
+    }
+
+    @Test
+    public void createInterfaceWithoutAddress() {
+        NetworkInterfaceContentFragment area = page.getContent();
+        NetworkInterfaceWizard wizard = area.addInterface();
+
+        boolean result = wizard
+                .name(INTERFACE_NO_ADDRESS_NAME)
+                .finish();
+
+        assertFalse("Window should not be closed", result);
+        verifier.verifyResource(DMR_INTERFACE_NO_ADDRESS, false);
+    }
+}
