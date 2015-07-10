46fb2be9e5df4f9ab1c2bb18afd1f2bb5c14ce57
Michal Krajcovic <mkrajcov@redhat.com>
2015-07-10 15:41:11 +0200

    Added JPA tests

---
+++ src/test/java/org/jboss/hal/testsuite/test/configuration/container/JPATestCase.java
@@ -0,0 +1,52 @@
+package org.jboss.hal.testsuite.test.configuration.container;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.hal.testsuite.cli.CliClient;
+import org.jboss.hal.testsuite.cli.CliClientFactory;
+import org.jboss.hal.testsuite.cli.CliConstants;
+import org.jboss.hal.testsuite.page.config.JPAPage;
+import org.jboss.hal.testsuite.test.category.Standalone;
+import org.jboss.hal.testsuite.test.util.ConfigAreaChecker;
+import org.jboss.hal.testsuite.util.Console;
+import org.jboss.hal.testsuite.util.ResourceVerifier;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author mkrajcov <mkrajcov@redhat.com>
+ */
+@RunWith(Arquillian.class)
+@Category(Standalone.class)
+public class JPATestCase {
+
+    private CliClient client = CliClientFactory.getClient();
+    private ResourceVerifier verifier = new ResourceVerifier(CliConstants.JPA_SUBSYSTEM_ADDRESS, client);
+    private ConfigAreaChecker checker = new ConfigAreaChecker(verifier);
+
+    @Drone
+    public WebDriver browser;
+
+    @Page
+    public JPAPage page;
+
+    @Before
+    public void before() {
+        Console.withBrowser(browser).refreshAndNavigate(JPAPage.class);
+    }
+
+    @Test
+    public void editDefaultDataSource() {
+        checker.editTextAndAssert(page, "defaultDataSource", "ds").dmrAttribute("default-datasource").invoke();
+    }
+
+    @Test
+    public void editPersistenceInheritance() {
+        checker.editSelectAndAssert(page, "inheritance", "DEEP").dmrAttribute("default-extended-persistence-inheritance").invoke();
+    }
+
+}
