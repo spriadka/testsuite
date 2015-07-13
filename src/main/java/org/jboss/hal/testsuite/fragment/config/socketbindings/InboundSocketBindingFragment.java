49a1f5e0729737f1c2e188b05c9133c188f99901
Michal Krajcovic <mkrajcov@redhat.com>
2015-07-13 11:02:33 +0200

    Updated Socket bindings test case

--- src/main/java/org/jboss/hal/testsuite/fragment/config/socketbindings/InboundSocketBindingFragment.java
+++ src/main/java/org/jboss/hal/testsuite/fragment/config/socketbindings/InboundSocketBindingFragment.java
@@ -1,47 +1,11 @@
 package org.jboss.hal.testsuite.fragment.config.socketbindings;
 
-import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.arquillian.graphene.findby.ByJQuery;
 import org.jboss.hal.testsuite.fragment.ConfigFragment;
-import org.jboss.hal.testsuite.fragment.formeditor.Editor;
-import org.jboss.hal.testsuite.util.Console;
-import org.jboss.hal.testsuite.util.PropUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 /**
  * @author mkrajcov <mkrajcov@redhat.com>
  */
 public class InboundSocketBindingFragment extends ConfigFragment {
-
-    private static final By MULTICAST_ANCHOR = ByJQuery.selector("a.header:has(td:contains('Multicast'):visible)");
-
-    public void editPortAndSave(String socketBindingName, String value){
-        getResourceManager().selectByName(socketBindingName);
-        Console.withBrowser(browser).pageDown();
-        Editor editor = edit();
-        editor.text("port", value);
-        WebElement saveButton = getButton(PropUtils.get("configarea.save.button.label"));
-        Graphene.waitGui().until().element(saveButton).is().visible();
-        save();
-    }
-
-    public void editMulticastAndSave(String socketBindingName, String multicastPort, String multicastAddress){
-        getResourceManager().selectByName(socketBindingName);
-        Editor editor = edit();
-        Console.withBrowser(browser).pageDown();
-        WebElement multicast = editor.getRoot().findElement(MULTICAST_ANCHOR);
-        Graphene.waitGui().until().element(multicast).is().visible();
-        multicast.click();
-
-        Console.withBrowser(browser).pageDown();
-        editor.text("multiCastPort", multicastPort);
-        editor.text("multiCastAddress", multicastAddress);
-        WebElement saveButton = getButton(PropUtils.get("configarea.save.button.label"));
-        Graphene.waitGui().until().element(saveButton).is().visible();
-        save();
-    }
-
     public InboundSocketBindingWizard addSocketBinding(){
         return getResourceManager().addResource(InboundSocketBindingWizard.class);
     }
