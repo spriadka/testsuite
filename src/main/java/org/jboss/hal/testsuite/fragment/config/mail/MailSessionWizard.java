3a4844b6880fccc107fb4a2144387fbc395bb34d
Patrik Cyprian <pcyprian@dhcp-10-40-4-252.brq.redhat.com>
2015-07-22 10:30:49 +0200

    Fixed mail test case.
    Updated mail test case for jboss7.0.0 DR6.

--- src/main/java/org/jboss/hal/testsuite/fragment/config/mail/MailSessionWizard.java
+++ src/main/java/org/jboss/hal/testsuite/fragment/config/mail/MailSessionWizard.java
@@ -8,9 +8,14 @@ import org.jboss.hal.testsuite.fragment.shared.modal.WizardWindow;
 public class MailSessionWizard extends WizardWindow{
 
     private static final String JNDI_NAME = "jndiName";
+    private static final String NAME = "name";
 
     public MailSessionWizard jndiName(String value){
         getEditor().text(JNDI_NAME, value);
+        return this;
+    }
+    public MailSessionWizard name(String value){
+        getEditor().text(NAME, value);
         return this;
     }
 }
--- src/test/java/org/jboss/hal/testsuite/test/configuration/connector/MailTestCase.java
+++ src/test/java/org/jboss/hal/testsuite/test/configuration/connector/MailTestCase.java
@@ -2,17 +2,18 @@ package org.jboss.hal.testsuite.test.configuration.connector;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jboss.arquillian.drone.api.annotation.Drone;
-import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.hal.testsuite.category.Standalone;
 import org.jboss.hal.testsuite.cli.CliClient;
 import org.jboss.hal.testsuite.cli.CliClientFactory;
+import org.jboss.hal.testsuite.cli.Library;
 import org.jboss.hal.testsuite.fragment.config.mail.MailServerFragment;
 import org.jboss.hal.testsuite.fragment.config.mail.MailServerWizard;
 import org.jboss.hal.testsuite.fragment.config.mail.MailSessionWizard;
 import org.jboss.hal.testsuite.fragment.config.mail.MailSessionsFragment;
+import org.jboss.hal.testsuite.fragment.shared.modal.ConfirmationWindow;
 import org.jboss.hal.testsuite.page.config.MailSessionsPage;
 import org.jboss.hal.testsuite.util.Console;
 import org.jboss.hal.testsuite.util.ResourceVerifier;
@@ -36,14 +37,15 @@ public class MailTestCase {
 
     private static final String MAIL_SESSION = "java:/mail_" + RandomStringUtils.randomAlphanumeric(5);
     private static final String MAIL_SESSION_INVALID = "%%%" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String MAIL_NAME = "n_" + RandomStringUtils.randomAlphanumeric(5);
     private static final String TYPE = "pop3";
     private static final String USERNAME = "un_" + RandomStringUtils.randomAlphanumeric(5);
     private static final String PASSWORD = "pw_" + RandomStringUtils.randomAlphanumeric(5);
     private static final String SOCKET_BINDING = "sb_" + RandomStringUtils.randomAlphanumeric(5);
     private static final String FROM = "from_" + RandomStringUtils.randomAlphanumeric(5);
 
-    private static final String DMR_SESSION = MAIL_SESSION_SUBSYSTEM_ADDRESS + "=\"" + MAIL_SESSION + "\"";
-    private static final String DMR_SESSION_INVALID = MAIL_SESSION_SUBSYSTEM_ADDRESS + "=\"" + MAIL_SESSION_INVALID + "\"";
+    private static final String DMR_SESSION = MAIL_SESSION_SUBSYSTEM_ADDRESS + "=" + MAIL_NAME + "";
+    private static final String DMR_SESSION_INVALID = MAIL_SESSION_SUBSYSTEM_ADDRESS + "=" + MAIL_SESSION_INVALID + "";
     private static final String DMR_SERVER = DMR_SESSION + "/server=" + TYPE;
 
     private static CliClient client = CliClientFactory.getClient();
@@ -57,10 +59,7 @@ public class MailTestCase {
 
     @Before
     public void before() {
-        browser.navigate().refresh();
-        Graphene.goTo(MailSessionsPage.class);
-        Console.withBrowser(browser).waitUntilLoaded();
-        Console.withBrowser(browser).maximizeWindow();
+        Console.withBrowser(browser).refreshAndNavigate(MailSessionsPage.class);
     }
 
     @AfterClass
@@ -72,10 +71,13 @@ public class MailTestCase {
 
     @Test
     public void createInvalidMailSession() {
+
+        page.select("Subsystems").select("Mail");
         MailSessionWizard wizard = page.getMailSessions().addMailSession();
 
         boolean result =
                 wizard.jndiName(MAIL_SESSION_INVALID)
+                        .name(MAIL_NAME)
                         .finish();
 
         assertFalse("Wizard window should not be closed.", result);
@@ -85,23 +87,26 @@ public class MailTestCase {
     @Test
     @InSequence(0)
     public void createMailSession() {
+        page.select("Subsystems").select("Mail");
         MailSessionsFragment fragment = page.getMailSessions();
         MailSessionWizard wizard = fragment.addMailSession();
 
         boolean result =
                 wizard.jndiName(MAIL_SESSION)
+                        .name(MAIL_NAME)
                         .finish();
 
         assertTrue("Wizard window should be closed.", result);
-        assertTrue("Mail session should be present in table", fragment.resourceIsPresent(MAIL_SESSION));
+
         verifier.verifyResource(DMR_SESSION, true);
     }
 
     @Test
     @InSequence(1)
     public void enableDebugOnMailSession() {
-        MailSessionsFragment fragment = page.getMailSessions();
-        fragment.editDebugAndSave(MAIL_SESSION, true);
+        page.select("Subsystems").select("Mail").select(MAIL_SESSION).option("Attributes");
+        Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).getEditor().checkbox("debug", true);
+        Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).clickButton("Save");
 
         verifier.verifyAttribute("debug", "true");
     }
@@ -109,8 +114,9 @@ public class MailTestCase {
     @Test
     @InSequence(2)
     public void disableDebugOnMailSession() {
-        MailSessionsFragment fragment = page.getMailSessions();
-        fragment.editDebugAndSave(MAIL_SESSION, false);
+        page.select("Subsystems").select("Mail").select(MAIL_SESSION).option("Attributes");
+        Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).getEditor().checkbox("debug", false);
+        Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).clickButton("Save");
 
         verifier.verifyAttribute("debug", "false");
     }
@@ -118,8 +124,9 @@ public class MailTestCase {
     @Test
     @InSequence(3)
     public void changeDefaultFromOnMailSession() {
-        MailSessionsFragment fragment = page.getMailSessions();
-        fragment.editFromAndSave(MAIL_SESSION, FROM);
+        page.select("Subsystems").select("Mail").select(MAIL_SESSION).option("Attributes");
+        Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).getEditor().text("from",FROM);
+        Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).clickButton("Save");
 
         verifier.verifyAttribute("from", FROM);
     }
@@ -127,7 +134,9 @@ public class MailTestCase {
     @Test
     @InSequence(4)
     public void createMailServer() {
-        MailServerFragment fragment = page.getMailServers(MAIL_SESSION);
+        page.select("Subsystems").select("Mail").view(MAIL_SESSION);
+        Library.letsSleep(1000);
+        MailServerFragment fragment = page.getSesionsServers();
         MailServerWizard wizard = fragment.addMailServer();
 
         boolean result =
@@ -140,13 +149,16 @@ public class MailTestCase {
 
         assertTrue("Window should be closed", result);
         assertTrue("Mail server should be present in table", fragment.resourceIsPresent(TYPE.toUpperCase()));
+
         verifier.verifyResource(DMR_SERVER, true);
     }
 
     @Test
     @InSequence(5)
     public void removeMailServer() {
-        MailServerFragment fragment = page.getMailServers(MAIL_SESSION);
+        page.select("Subsystems").select("Mail").view(MAIL_SESSION);
+        Library.letsSleep(1000);
+        MailServerFragment fragment = page.getSesionsServers();
         fragment.removeAndConfirm(TYPE);
 
         verifier.verifyResource(DMR_SERVER, false);
@@ -155,8 +167,7 @@ public class MailTestCase {
     @Test
     @InSequence(6)
     public void removeMailSession() {
-        MailSessionsFragment fragment = page.getMailSessions();
-        fragment.removeAndConfirm(MAIL_SESSION);
+        page.select("Subsystems").select("Mail").select(MAIL_SESSION).option("Remove");
 
         verifier.verifyResource(DMR_SESSION, false);
     }
