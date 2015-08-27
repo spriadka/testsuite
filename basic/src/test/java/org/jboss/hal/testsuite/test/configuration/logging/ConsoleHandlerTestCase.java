8989f5880eb3e8d191b399e1ddcc6710ab9bc2d6
Patrik Cyprian <pcyprian@dhcp-10-40-4-252.brq.redhat.com>
2015-08-27 12:22:14 +0200

    Creating new logging package with test cases for handlers,categories,formatters and root logger.Updated loggingPage.
    Creating new test cases for logging subsystem.
    Added some methods in logging page  to change views and tabs in logging subsystem.Removed unused cliClient.
    Checkstyle fixes.

---
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/logging/ConsoleHandlerTestCase.java
@@ -0,0 +1,145 @@
+package org.jboss.hal.testsuite.test.configuration.logging;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.dmr.ModelNode;
+import org.jboss.hal.testsuite.category.Standalone;
+import org.jboss.hal.testsuite.dmr.Dispatcher;
+import org.jboss.hal.testsuite.dmr.ResourceAddress;
+import org.jboss.hal.testsuite.dmr.ResourceVerifier;
+import org.jboss.hal.testsuite.finder.Application;
+import org.jboss.hal.testsuite.finder.FinderNames;
+import org.jboss.hal.testsuite.finder.FinderNavigation;
+import org.jboss.hal.testsuite.fragment.ConfigFragment;
+import org.jboss.hal.testsuite.page.config.LoggingPage;
+import org.jboss.hal.testsuite.page.config.StandaloneConfigEntryPoint;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by pcyprian on 27.8.15.
+ */
+@RunWith(Arquillian.class)
+@Category(Standalone.class)
+public class ConsoleHandlerTestCase {
+    private static final String CONSOLEHANDLER = "consoleHandler";
+
+    private FinderNavigation navigation;
+
+    private ModelNode path = new ModelNode("/subsystem=logging/console-handler="+ CONSOLEHANDLER);
+    private ResourceAddress address = new ResourceAddress(path);
+    Dispatcher dispatcher = new Dispatcher();
+    ResourceVerifier verifier = new ResourceVerifier(dispatcher);
+
+    @Drone
+    private WebDriver browser;
+    @Page
+    private LoggingPage page;
+
+    @Before
+    public void before() {
+        navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
+                .addAddress(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
+                .addAddress(FinderNames.SUBSYSTEM, "Logging");
+
+        navigation.selectRow().invoke(FinderNames.VIEW);
+        Application.waitUntilVisible();
+
+        page.switchToHandlerTab();
+    }
+
+    @Test
+    @InSequence(0)
+    public void addConsoleHandler(){
+        page.addConsoleHandler(CONSOLEHANDLER, "PATTERN");
+
+        verifier.verifyResource(address, true);
+    }
+
+    @Test
+    @InSequence(1)
+    public void updateConsoleHandlerLevel(){
+        page.getResourceManager().getResourceTable().selectRowByText(0,CONSOLEHANDLER);
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().select("level", "DEBUG");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "level", "DEBUG");
+    }
+
+    @Test
+    @InSequence(2)
+    public void updateConsoleHandlerNamedFormatter(){
+        page.getResourceManager().getResourceTable().selectRowByText(0,CONSOLEHANDLER);
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().text("named-formatter", "");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "named-formatter" , "undefined");
+    }
+
+    @Test
+    @InSequence(3)
+    public void updateConsoleHandlerTarget(){
+        page.getResourceManager().getResourceTable().selectRowByText(0,CONSOLEHANDLER);
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().select("target", "console");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "target" , "console");
+    }
+
+    @Test
+    @InSequence(4)
+    public void updateConsoleHandlerAutoflush(){
+        page.getResourceManager().getResourceTable().selectRowByText(0,CONSOLEHANDLER);
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().checkbox("autoflush", false);
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "autoflush" , false);
+    }
+
+    @Test
+    @InSequence(5)
+    public void updateConsoleHandlerFormatter(){
+        page.getResourceManager().getResourceTable().selectRowByText(0,CONSOLEHANDLER);
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().text("formatter", "");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "formatter" , "%d{HH:mm:ss,SSS} %-5p [%c] (%t) %s%e%n");
+    }
+
+    @Test
+    @InSequence(6)
+    public void removeConsoleHandler(){
+        page.getResourceManager().getResourceTable().selectRowByText(0,CONSOLEHANDLER);
+        page.remove();
+
+        verifier.verifyResource(address, false);
+    }
+
+}
---
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/logging/SyslogTestCase.java
@@ -0,0 +1,128 @@
+package org.jboss.hal.testsuite.test.configuration.logging;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.dmr.ModelNode;
+import org.jboss.hal.testsuite.category.Standalone;
+import org.jboss.hal.testsuite.dmr.Dispatcher;
+import org.jboss.hal.testsuite.dmr.ResourceAddress;
+import org.jboss.hal.testsuite.dmr.ResourceVerifier;
+import org.jboss.hal.testsuite.finder.Application;
+import org.jboss.hal.testsuite.finder.FinderNames;
+import org.jboss.hal.testsuite.finder.FinderNavigation;
+import org.jboss.hal.testsuite.fragment.ConfigFragment;
+import org.jboss.hal.testsuite.page.config.LoggingPage;
+import org.jboss.hal.testsuite.page.config.StandaloneConfigEntryPoint;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by pcyprian on 27.8.15.
+ */
+@RunWith(Arquillian.class)
+@Category(Standalone.class)
+public class SyslogTestCase {
+    private static final String SYSLOGHANDLER = "syslogHandler";
+
+    private FinderNavigation navigation;
+
+    private ModelNode path = new ModelNode("/subsystem=logging/syslog-handler="+ SYSLOGHANDLER);
+    private ResourceAddress address = new ResourceAddress(path);
+    Dispatcher dispatcher = new Dispatcher();
+    ResourceVerifier verifier = new ResourceVerifier(dispatcher);
+
+    @Drone
+    private WebDriver browser;
+    @Page
+    private LoggingPage page;
+
+    @Before
+    public void before() {
+        navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
+                .addAddress(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
+                .addAddress(FinderNames.SUBSYSTEM, "Logging");
+
+        navigation.selectRow().invoke(FinderNames.VIEW);
+        Application.waitUntilVisible();
+
+        page.switchToHandlerTab();
+        page.switchToSyslog();
+    }
+
+    @Test
+    @InSequence(0)
+    public void addSyslogHandler(){
+        page.addFileHandler(SYSLOGHANDLER);
+
+        verifier.verifyResource(address, true);
+    }
+
+    @Test //https://issues.jboss.org/browse/HAL-813
+    @InSequence(1)
+    public void updateSyslogHandlerPortWrongValue(){
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().text("port", "-1");
+        boolean finished = editPanelFragment.save();
+
+        assertFalse("Config should not be saved and closed. -1 is invalid value", finished);
+        verifier.verifyAttribute(address, "port", "514");
+    }
+
+    @Test
+    @InSequence(2)
+    public void updateSyslogHandlerPort() {
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().text("port", "0");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should not be saved and closed.", finished);
+        verifier.verifyAttribute(address, "port", "0");
+    }
+
+    @Test
+    @InSequence(3)
+    public void updateSyslogHandlerFacolity(){
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().select("facility", "local-use-1");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "facility", "local-use-1");
+    }
+
+
+    @Test
+    @InSequence(4)
+    public void updateSyslogHandlerFormat(){
+        page.edit();
+        ConfigFragment editPanelFragment = page.getConfigFragment();
+
+        editPanelFragment.getEditor().select("syslog-format","RFC3164");
+        boolean finished = editPanelFragment.save();
+
+        assertTrue("Config should be saved and closed.", finished);
+        verifier.verifyAttribute(address, "syslog-format", "RFC3164");
+    }
+
+    @Test
+    @InSequence(5)
+    public void removeSyslogHandler(){
+        page.remove();
+
+        verifier.verifyResource(address, false);
+    }
+}
