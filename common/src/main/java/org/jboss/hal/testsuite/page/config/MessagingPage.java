e1b8c1911d64ac7bf431f64c5b3b344ac3196944
Patrik Cyprian <pcyprian@dhcp-10-40-4-252.brq.redhat.com>
2015-09-03 14:48:21 +0200

    Migreated messaging clustering test cases.
    Created broadcast/discovery group test case and cluster connection test case.

---
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/MessagingPage.java
@@ -0,0 +1,107 @@
+package org.jboss.hal.testsuite.page.config;
+
+import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.arquillian.graphene.findby.ByJQuery;
+import org.jboss.hal.testsuite.cli.TimeoutException;
+import org.jboss.hal.testsuite.finder.Application;
+import org.jboss.hal.testsuite.finder.FinderNames;
+import org.jboss.hal.testsuite.finder.FinderNavigation;
+import org.jboss.hal.testsuite.fragment.ConfigFragment;
+import org.jboss.hal.testsuite.fragment.formeditor.Editor;
+import org.jboss.hal.testsuite.fragment.shared.modal.ConfirmationWindow;
+import org.jboss.hal.testsuite.page.ConfigPage;
+import org.jboss.hal.testsuite.util.ConfigUtils;
+import org.jboss.hal.testsuite.util.Console;
+import org.jboss.hal.testsuite.util.PropUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by pcyprian on 2.9.15.
+ */
+public class MessagingPage extends ConfigPage {
+
+    private FinderNavigation navigation;
+
+    public ConfigFragment getConfigFragment() {
+        WebElement editPanel = browser.findElement(ByJQuery.selector(".master_detail-detail:visible"));
+        return  Graphene.createPageFragment(ConfigFragment.class, editPanel);
+    }
+
+    public ConfigFragment getWindowFragment() {
+        WebElement editPanel = browser.findElement(By.className("default-window-content"));
+        return  Graphene.createPageFragment(ConfigFragment.class, editPanel);
+    }
+
+    public void navigateToMessaging() {
+        if (ConfigUtils.isDomain()) {
+            navigation = new FinderNavigation(browser, DomainConfigEntryPoint.class)
+                    .addAddress(FinderNames.CONFIGURATION, FinderNames.PROFILES)
+                    .addAddress(FinderNames.PROFILE, "full-ha")
+                    .addAddress(FinderNames.SUBSYSTEM, "Messaging")
+                    .addAddress("Messaging Provider", "default");
+        } else {
+            navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
+                    .addAddress(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
+                    .addAddress(FinderNames.SUBSYSTEM, "Messaging")
+                    .addAddress("Messaging Provider", "default");
+        }
+    }
+
+    public void selectView(String view) {
+        navigation.selectRow().invoke(view);
+        Application.waitUntilVisible();
+    }
+
+    public void switchToDiscovery() {
+        //switchView("Discovery");
+        WebElement viewPanel = browser.findElement(By.className("paged-view-navigation-container"));
+        WebElement editLink = viewPanel.findElement(By.linkText("Discovery"));
+        editLink.click();
+    }
+
+    public void switchToConnections() {
+        switchView("Connections");
+    }
+
+    public void selectInTable(String value, int column) {
+        getResourceManager().getResourceTable().selectRowByText(column, value);
+    }
+
+    public void addBroadcastGroup(String name, String binding) {
+        clickButton("Add");
+        getWindowFragment().getEditor().text("name",name);
+        getWindowFragment().getEditor().text("socketBinding", binding);
+        getWindowFragment().clickButton("Save");
+    }
+
+
+    public void addClusterConnection(String name, String dg, String connectorName ,String connectorAddress) {
+        clickButton("Add");
+        getWindowFragment().getEditor().text("name", name);
+        getWindowFragment().getEditor().text("discoveryGroupName",dg);
+        getWindowFragment().getEditor().text("connectorRef",connectorName);
+        getWindowFragment().getEditor().text("clusterConnectionAddress", connectorAddress);
+        getWindowFragment().clickButton("Save");
+    }
+
+    public Editor edit() {
+        WebElement button = getEditButton();
+        button.click();
+        Graphene.waitGui().until().element(button).is().not().visible();
+        return getConfig().getEditor();
+    }
+
+    private WebElement getEditButton() {
+        By selector = ByJQuery.selector("." + PropUtils.get("configarea.edit.button.class") + ":visible");
+        return getContentRoot().findElement(selector);
+    }
+
+    public void remove() {
+        clickButton("Remove");
+        try {
+            Console.withBrowser(browser).openedWindow(ConfirmationWindow.class).confirm();
+        } catch (TimeoutException ignored) {
+        }
+    }
+}
