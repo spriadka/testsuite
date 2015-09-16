ed3661708dc7597b8f51ac412070fdfa7f8801bd
Jan Kasik <jkasik@redhat.com>
2015-09-16 10:03:33 +0200

    Udated undertow abstract test case

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/UndertowTestCaseAbstract.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/UndertowTestCaseAbstract.java
@@ -8,6 +8,7 @@ import org.jboss.hal.testsuite.cli.DomainManager;
 import org.jboss.hal.testsuite.dmr.AddressTemplate;
 import org.jboss.hal.testsuite.dmr.DefaultContext;
 import org.jboss.hal.testsuite.dmr.Dispatcher;
+import org.jboss.hal.testsuite.dmr.Operation;
 import org.jboss.hal.testsuite.dmr.ResourceAddress;
 import org.jboss.hal.testsuite.dmr.ResourceVerifier;
 import org.jboss.hal.testsuite.dmr.StatementContext;
@@ -19,6 +20,8 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Jan Kasik <jkasik@redhat.com>
@@ -26,10 +29,11 @@ import java.io.IOException;
  */
 public class UndertowTestCaseAbstract {
 
-    protected Dispatcher dispatcher;
+    protected static Dispatcher dispatcher;
     protected ResourceVerifier verifier;
-    protected StatementContext context;
-    protected AddressTemplate undertowAddressTemplate = AddressTemplate.of("{default.profile}/subsystem=undertow");
+    protected static StatementContext context;
+    protected static AddressTemplate undertowAddressTemplate = AddressTemplate.of("{default.profile}/subsystem=undertow");
+    protected static AddressTemplate httpServerTemplate = undertowAddressTemplate.append("/server=*");
 
     @Drone
     protected WebDriver browser;
@@ -66,6 +70,12 @@ public class UndertowTestCaseAbstract {
         verifier.verifyAttribute(address, attributeName, value);
     }
 
+    public void editTextAreaAndVerify(ResourceAddress address, String identifier, String attributeName, String[] values) throws IOException, InterruptedException {
+        page.getConfigFragment().editTextAndSave(identifier, String.join("\n", values));
+        reloadIfRequiredAndWaitForRunning();
+        verifier.verifyAttribute(address, attributeName, values);
+    }
+
     protected void reloadIfRequiredAndWaitForRunning() {
         final int timeout = 60000;
         if (ConfigUtils.isDomain()) {
@@ -82,5 +92,16 @@ public class UndertowTestCaseAbstract {
         config.cancel();
     }
 
+    protected static String createHTTPServer(Dispatcher dispatcher) {
+        String name = org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(6);
+        ResourceAddress address = httpServerTemplate.resolve(context, name);
+        dispatcher.execute(new Operation.Builder("add", address).build());
+        return name;
+    }
+
+    protected static void removeHTTPServer(Dispatcher dispatcher, String httpServer) {
+        ResourceAddress address = httpServerTemplate.resolve(context, httpServer);
+        dispatcher.execute(new Operation.Builder("remove", address).build());
+    }
 
 }
