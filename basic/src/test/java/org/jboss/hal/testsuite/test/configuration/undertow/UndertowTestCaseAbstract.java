715247255d709c886cd96a78f08be755d14adb62
Jan Kasik <jkasik@redhat.com>
2015-10-01 08:20:56 +0200

    Updated undertow subsystem abstract test case

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/UndertowTestCaseAbstract.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/undertow/UndertowTestCaseAbstract.java
@@ -3,37 +3,43 @@ package org.jboss.hal.testsuite.test.configuration.undertow;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
-import org.jboss.hal.testsuite.cli.CliClientFactory;
-import org.jboss.hal.testsuite.cli.DomainManager;
+import org.jboss.hal.testsuite.cli.Library;
 import org.jboss.hal.testsuite.dmr.AddressTemplate;
 import org.jboss.hal.testsuite.dmr.DefaultContext;
 import org.jboss.hal.testsuite.dmr.Dispatcher;
-import org.jboss.hal.testsuite.dmr.Operation;
 import org.jboss.hal.testsuite.dmr.ResourceAddress;
 import org.jboss.hal.testsuite.dmr.ResourceVerifier;
 import org.jboss.hal.testsuite.dmr.StatementContext;
 import org.jboss.hal.testsuite.fragment.config.undertow.UndertowFragment;
 import org.jboss.hal.testsuite.page.config.UndertowPage;
-import org.jboss.hal.testsuite.util.ConfigUtils;
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Jan Kasik <jkasik@redhat.com>
  *         Created on 15.9.15.
  */
-public class UndertowTestCaseAbstract {
+public abstract class UndertowTestCaseAbstract {
 
     protected static Dispatcher dispatcher;
-    protected ResourceVerifier verifier;
-    protected static StatementContext context;
+    protected static ResourceVerifier verifier;
+    protected static UndertowOperations operations;
+    protected static StatementContext context = new DefaultContext();
     protected static AddressTemplate undertowAddressTemplate = AddressTemplate.of("{default.profile}/subsystem=undertow");
     protected static AddressTemplate httpServerTemplate = undertowAddressTemplate.append("/server=*");
+    protected static AddressTemplate servletContainerTemplate = undertowAddressTemplate.append("/servlet-container=*");
+
+    //shared valid values
+    protected static String WORKER_VALUE_VALID;
+    protected static String BUFFER_POOL_VALUE_VALID;
+    protected String SOCKET_BINDING_VALUE_VALID = "https";
+    protected String NUMERIC_VALID = "25";
+    protected String NUMERIC_INVALID = "25dfs";
+    protected String BUFFER_CACHE_VALUE_VALID = "";
 
     @Drone
     protected WebDriver browser;
@@ -41,16 +47,26 @@ public class UndertowTestCaseAbstract {
     @Page
     protected UndertowPage page;
 
-    @Before
-    public void mainBefore() {
+    @BeforeClass
+    public static void mainSetUp() {
         dispatcher = new Dispatcher();
         verifier = new ResourceVerifier(dispatcher);
-        context = new DefaultContext();
+        operations = new UndertowOperations(dispatcher);
+        WORKER_VALUE_VALID = operations.createWorker();
+        BUFFER_POOL_VALUE_VALID = operations.createBufferPool();
+    }
+
+    @AfterClass
+    public static void mainTearDown() {
+        operations.removeWorker(WORKER_VALUE_VALID);
+        operations.removeBufferPool(BUFFER_POOL_VALUE_VALID);
+        dispatcher.close();
     }
 
     protected void editTextAndVerify(ResourceAddress address, String identifier, String attributeName, String value) throws IOException, InterruptedException {
         page.getConfigFragment().editTextAndSave(identifier, value);
-        reloadIfRequiredAndWaitForRunning();
+        Library.letsSleep(5000);
+        UndertowOperations.reloadIfRequiredAndWaitForRunning();
         verifier.verifyAttribute(address, attributeName, value);
     }
 
@@ -60,29 +76,20 @@ public class UndertowTestCaseAbstract {
 
     protected void editCheckboxAndVerify(ResourceAddress address, String identifier, String attributeName, Boolean value) throws IOException, InterruptedException {
         page.getConfigFragment().editCheckboxAndSave(identifier, value);
-        reloadIfRequiredAndWaitForRunning();
+        UndertowOperations.reloadIfRequiredAndWaitForRunning();
         verifier.verifyAttribute(address, attributeName, value.toString());
     }
 
     public void selectOptionAndVerify(ResourceAddress address, String identifier, String attributeName, String value) throws IOException, InterruptedException {
         page.getConfigFragment().selectOptionAndSave(identifier, value);
-        reloadIfRequiredAndWaitForRunning();
+        UndertowOperations.reloadIfRequiredAndWaitForRunning();
         verifier.verifyAttribute(address, attributeName, value);
     }
 
     public void editTextAreaAndVerify(ResourceAddress address, String identifier, String attributeName, String[] values) throws IOException, InterruptedException {
         page.getConfigFragment().editTextAndSave(identifier, String.join("\n", values));
-        reloadIfRequiredAndWaitForRunning();
+        UndertowOperations.reloadIfRequiredAndWaitForRunning();
         verifier.verifyAttribute(address, attributeName, values);
-    }
-
-    protected void reloadIfRequiredAndWaitForRunning() {
-        final int timeout = 60000;
-        if (ConfigUtils.isDomain()) {
-            new DomainManager(CliClientFactory.getClient()).reloadIfRequiredAndWaitUntilRunning(timeout);
-        } else {
-            CliClientFactory.getClient().reload();
-        }
     }
 
     protected void verifyIfErrorAppears(String identifier, String value) {
@@ -92,16 +99,6 @@ public class UndertowTestCaseAbstract {
         config.cancel();
     }
 
-    protected static String createHTTPServer(Dispatcher dispatcher) {
-        String name = org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(6);
-        ResourceAddress address = httpServerTemplate.resolve(context, name);
-        dispatcher.execute(new Operation.Builder("add", address).build());
-        return name;
-    }
 
-    protected static void removeHTTPServer(Dispatcher dispatcher, String httpServer) {
-        ResourceAddress address = httpServerTemplate.resolve(context, httpServer);
-        dispatcher.execute(new Operation.Builder("remove", address).build());
-    }
 
 }
