7dcce824d59e6081b5023fc5401ecf75ccde7659
Jan Kasik <jkasik@redhat.com>
2015-10-05 11:21:50 +0200

    Updated undertow fragment

--- common/src/main/java/org/jboss/hal/testsuite/fragment/config/undertow/UndertowFragment.java
+++ common/src/main/java/org/jboss/hal/testsuite/fragment/config/undertow/UndertowFragment.java
@@ -2,9 +2,12 @@ package org.jboss.hal.testsuite.fragment.config.undertow;
 
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.findby.ByJQuery;
+import org.jboss.hal.testsuite.cli.Library;
 import org.jboss.hal.testsuite.fragment.ConfigFragment;
-import org.jboss.hal.testsuite.util.PropUtils;
+import org.jboss.hal.testsuite.fragment.formeditor.Editor;
+import org.jboss.hal.testsuite.util.Console;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -12,8 +15,6 @@ import org.openqa.selenium.WebElement;
  *         Created on 15.9.15.
  */
 public class UndertowFragment extends ConfigFragment {
-
-    private static final By CONTENT_ROOT = ByJQuery.selector("." + PropUtils.get("page.content.rhs.class") + ":visible");
 
     public Boolean isErrorShowedInForm() {
         By selector = ByJQuery.selector("div.form-item-error-desc:visible");
@@ -30,8 +31,38 @@ public class UndertowFragment extends ConfigFragment {
     }
 
     public Boolean editTextAndSave(String identifier, String value) {
-        edit().text(identifier, value);
+        Editor editor = edit();
+        editor.text(identifier, value);
         return save();
+    }
+
+    public void editTextHumanly(Editor editor, String identifier, String value) {
+        WebElement input = editor.getText(identifier);
+        while (!input.isDisplayed()) {
+            Console.withBrowser(browser).pageDown();
+        }
+        input.click();
+        int length = input.getAttribute("value").length();
+        input.click();
+
+        //clear one by one
+        for (int i = 0; i < length; i++) {
+            input.sendKeys(Keys.DELETE);
+            input.click();
+            Library.letsSleep(100);
+        }
+
+        Graphene.waitGui().until().element(input).value().equalTo("");
+        input.click();
+
+        //write one by one
+        for (char c : value.toCharArray()) {
+            input.sendKeys(Character.toString(c));
+            input.click();
+            Library.letsSleep(100);
+        }
+        input.click();
+        Graphene.waitGui().until().element(input).value().equalTo(value);
     }
 
     public Boolean editCheckboxAndSave(String identifier, Boolean value) {
