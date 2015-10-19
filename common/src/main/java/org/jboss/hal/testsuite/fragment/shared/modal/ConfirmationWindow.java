0c6fbcd064e0fa8d10468c9cf412968b7c07bd49
Pavel Jelinek <pjelinek@redhat.com>
2015-10-19 15:18:10 +0200

    Change confirm method to be able to use for following state assert.

--- common/src/main/java/org/jboss/hal/testsuite/fragment/shared/modal/ConfirmationWindow.java
+++ common/src/main/java/org/jboss/hal/testsuite/fragment/shared/modal/ConfirmationWindow.java
@@ -1,17 +1,19 @@
 package org.jboss.hal.testsuite.fragment.shared.modal;
 
 import org.jboss.hal.testsuite.fragment.WindowFragment;
+import org.jboss.hal.testsuite.fragment.WindowState;
 import org.jboss.hal.testsuite.util.PropUtils;
 
 /**
  * Created by jcechace on 21/02/14.
  */
 public class ConfirmationWindow extends WindowFragment {
-    public void confirm() {
+    public WindowState confirm() {
         String label = PropUtils.get("modals.confirmation.confirm.label");
         clickButton(label);
 
         waitUntilClosed();
         closed = true;
+        return new WindowState(this);
     }
 }
