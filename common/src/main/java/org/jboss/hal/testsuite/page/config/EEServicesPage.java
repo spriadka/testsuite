624ef15df0bd674c6b4fb62d5ef43cd17d42483b
Pavel Jelinek <pjelinek@redhat.com>
2015-10-27 17:38:49 +0100

    Removing HAL-836 workaround.

--- common/src/main/java/org/jboss/hal/testsuite/page/config/EEServicesPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/EEServicesPage.java
@@ -1,14 +1,12 @@
 package org.jboss.hal.testsuite.page.config;
 
 import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.arquillian.graphene.findby.ByJQuery;
 import org.jboss.hal.testsuite.finder.Application;
 import org.jboss.hal.testsuite.finder.FinderNames;
 import org.jboss.hal.testsuite.finder.FinderNavigation;
 import org.jboss.hal.testsuite.fragment.config.ee.EEConfigFragment;
 import org.jboss.hal.testsuite.page.Navigatable;
 import org.jboss.hal.testsuite.util.ConfigUtils;
-import org.jboss.hal.testsuite.util.Console;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -37,13 +35,6 @@ public class EEServicesPage extends ConfigurationPage implements Navigatable {
         }
         navigation.selectRow().invoke(FinderNames.VIEW);
         Application.waitUntilVisible();
-        //TODO: remove this after HAL-836 is resolved
-        By selector = ByJQuery.selector(".link-bar-first");
-        browser.findElement(selector).click();
-        Console.withBrowser(browser).waitUntilLoaded();
-        view("EE");
-        Console.withBrowser(browser).waitUntilLoaded();
-        //END OF REMOVE
         switchTab("Services");
     }
 
