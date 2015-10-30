fa6e4590b88319cbc0a1dae8b5f157bf1659b355
Jan Kasik <jkasik@redhat.com>
2015-10-30 14:24:52 +0100

    Added failure logging to dispatcher

--- common/src/main/java/org/jboss/hal/testsuite/dmr/Dispatcher.java
+++ common/src/main/java/org/jboss/hal/testsuite/dmr/Dispatcher.java
@@ -119,6 +119,8 @@ public class Dispatcher {
                 }
             }
             log.info("Operation {} finished with {}", operation, response.isSuccessful() ? "success" : "failure");
+            if (!response.isSuccessful())
+                log.info("Operation {} failed because {}.", operation, response.getFailureDescription().asString());
             return response;
 
         } catch (IOException e) {
