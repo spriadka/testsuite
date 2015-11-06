7f380040aa4e25213f98c9fdcddec3d6b7b0492c
Jan Kasik <jkasik@redhat.com>
2015-11-06 12:52:11 +0100

    Fixed navigation in network interfaces testcase

--- common/src/main/java/org/jboss/hal/testsuite/page/config/NetworkInterfacesPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/NetworkInterfacesPage.java
@@ -2,8 +2,11 @@ package org.jboss.hal.testsuite.page.config;
 
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.page.Location;
+import org.jboss.hal.testsuite.finder.FinderNames;
+import org.jboss.hal.testsuite.finder.FinderNavigation;
 import org.jboss.hal.testsuite.fragment.config.interfaces.NetworkInterfaceContentFragment;
 import org.jboss.hal.testsuite.page.ConfigPage;
+import org.jboss.hal.testsuite.page.Navigatable;
 import org.jboss.hal.testsuite.util.PropUtils;
 import org.openqa.selenium.By;
 
@@ -11,11 +14,18 @@ import org.openqa.selenium.By;
  * @author mkrajcov <mkrajcov@redhat.com>
  */
 @Location("#profile/interfaces")
-public class NetworkInterfacesPage extends ConfigPage {
+public class NetworkInterfacesPage extends ConfigPage implements Navigatable {
 
     private static final By CONTENT = By.id(PropUtils.get("page.content.area.id"));
 
     public NetworkInterfaceContentFragment getContent() {
         return Graphene.createPageFragment(NetworkInterfaceContentFragment.class, getContentRoot().findElement(CONTENT));
+    }
+
+    @Override
+    public void navigate() {
+        new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
+                .addAddress(FinderNames.CONFIGURATION, "Interfaces")
+                .selectRow().invoke(FinderNames.VIEW);
     }
 }
