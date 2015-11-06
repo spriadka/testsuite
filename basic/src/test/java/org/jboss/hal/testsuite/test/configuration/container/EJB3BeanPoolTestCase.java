d734bfbb7aa91ea1fbd65388a30e0346f8305e9b
Patrik Cyprian <pcyprian@dhcp-10-40-4-252.brq.redhat.com>
2015-11-06 11:55:11 +0100

    Fixed navigation in EJB3 test cases.

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/container/EJB3BeanPoolTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/container/EJB3BeanPoolTestCase.java
@@ -9,10 +9,15 @@ import org.jboss.hal.testsuite.category.Standalone;
 import org.jboss.hal.testsuite.cli.CliClient;
 import org.jboss.hal.testsuite.cli.CliClientFactory;
 import org.jboss.hal.testsuite.cli.CliConstants;
+import org.jboss.hal.testsuite.finder.Application;
+import org.jboss.hal.testsuite.finder.FinderNames;
+import org.jboss.hal.testsuite.finder.FinderNavigation;
 import org.jboss.hal.testsuite.fragment.config.container.EJB3BeanPoolsFragment;
+import org.jboss.hal.testsuite.page.config.DomainConfigurationPage;
 import org.jboss.hal.testsuite.page.config.EJB3Page;
+import org.jboss.hal.testsuite.page.config.StandaloneConfigurationPage;
 import org.jboss.hal.testsuite.test.util.ConfigAreaChecker;
-import org.jboss.hal.testsuite.util.Console;
+import org.jboss.hal.testsuite.util.ConfigUtils;
 import org.jboss.hal.testsuite.util.ResourceVerifier;
 import org.junit.Assert;
 import org.junit.Before;
@@ -33,6 +38,7 @@ public class EJB3BeanPoolTestCase {
     private CliClient client = CliClientFactory.getClient();
     private ResourceVerifier verifier = new ResourceVerifier(CliConstants.EJB3_BEAN_POOL_ADDRESS + "=" + BEAN_POOL_NAME, client);
     private ConfigAreaChecker checker = new ConfigAreaChecker(verifier);
+    private FinderNavigation finderNavigation;
 
     @Drone
     public WebDriver browser;
@@ -42,7 +48,19 @@ public class EJB3BeanPoolTestCase {
 
     @Before
     public void before() {
-        Console.withBrowser(browser).refreshAndNavigate(EJB3Page.class);
+        //Console.withBrowser(browser).refreshAndNavigate(EJB3Page.class);
+        if (ConfigUtils.isDomain()) {
+            finderNavigation = new FinderNavigation(browser, DomainConfigurationPage.class)
+                    .addAddress(FinderNames.CONFIGURATION, FinderNames.PROFILES)
+                    .addAddress(FinderNames.PROFILE, "full")
+                    .addAddress(FinderNames.SUBSYSTEM, "EJB 3");
+        } else {
+            finderNavigation = new FinderNavigation(browser, StandaloneConfigurationPage.class)
+                    .addAddress(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
+                    .addAddress(FinderNames.SUBSYSTEM, "EJB 3");
+        }
+        finderNavigation.selectRow().invoke("View");
+        Application.waitUntilVisible();
         fragment = page.beanPools();
     }
 
