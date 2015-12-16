f080992788fc6827f5bc800f4ff490dc7ebbc681
Jan Kasik <jkasik@redhat.com>
2015-12-16 08:58:48 +0100

    Added creaper to RootLoggerTestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/logging/RootLoggerTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/logging/RootLoggerTestCase.java
@@ -3,25 +3,17 @@ package org.jboss.hal.testsuite.test.configuration.logging;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
-import org.jboss.dmr.ModelNode;
 import org.jboss.hal.testsuite.category.Standalone;
-import org.jboss.hal.testsuite.dmr.Dispatcher;
-import org.jboss.hal.testsuite.dmr.ResourceAddress;
-import org.jboss.hal.testsuite.dmr.ResourceVerifier;
-import org.jboss.hal.testsuite.finder.Application;
-import org.jboss.hal.testsuite.finder.FinderNames;
-import org.jboss.hal.testsuite.finder.FinderNavigation;
+import org.jboss.hal.testsuite.creaper.ResourceVerifier;
 import org.jboss.hal.testsuite.fragment.ConfigFragment;
+import org.jboss.hal.testsuite.fragment.formeditor.Editor;
 import org.jboss.hal.testsuite.page.config.LoggingPage;
-import org.jboss.hal.testsuite.page.config.StandaloneConfigEntryPoint;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.wildfly.extras.creaper.core.online.operations.Address;
 
 import static org.junit.Assert.assertTrue;
 
@@ -30,25 +22,9 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(Arquillian.class)
 @Category(Standalone.class)
-public class RootLoggerTestCase {
-
-    private FinderNavigation navigation;
-
-    private ModelNode path = new ModelNode("/subsystem=logging/root-logger=ROOT");
-    private ResourceAddress address = new ResourceAddress(path);
-    private static Dispatcher dispatcher;
-    private static ResourceVerifier verifier;
-
-    @BeforeClass
-    public static void setUp() {
-        dispatcher = new Dispatcher();
-        verifier  = new ResourceVerifier(dispatcher);
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        dispatcher.close();
-    }
+public class RootLoggerTestCase extends LoggingAbstractTestCase {
+    //TODO add command for attribute backup and restore
+    private static final Address ROOT_LOGGER_ADDRESS = LOGGING_SUBSYSTEM.and("root-logger", "ROOT");
 
     @Drone
     private WebDriver browser;
@@ -57,42 +33,34 @@ public class RootLoggerTestCase {
 
     @Before
     public void before() {
-        navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
-                .addAddress(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
-                .addAddress(FinderNames.SUBSYSTEM, "Logging");
-
-        navigation.selectRow().invoke(FinderNames.VIEW);
-        Application.waitUntilVisible();
+        page.navigate();
     }
 
     @Test
-    @InSequence(0)
-    public void updateRootLoggerAttributes() {
-        page.getContentRoot();
-        page.edit();
+    public void updateRootLoggerAttributes() throws Exception {
         ConfigFragment editPanelFragment = page.getConfigFragment();
+        Editor editor = editPanelFragment.edit();
 
-        editPanelFragment.getEditor().text("handlers", "");
-        editPanelFragment.getEditor().select("level", "WARN");
+        editor.text("handlers", "");
+        editor.select("level", "WARN");
         boolean finished = editPanelFragment.save();
 
         assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "handlers", "undefined", 500);
-        verifier.verifyAttribute(address, "level", "WARN", 500);
+        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttributeIsUndefined("handlers");
+        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttribute("level", "WARN");
     }
 
     @Test
-    @InSequence(1)
-    public void setRootLoggerAttributesToDefault() {
-        page.edit();
+    public void setRootLoggerAttributesToDefault() throws Exception {
         ConfigFragment editPanelFragment = page.getConfigFragment();
+        Editor editor = editPanelFragment.edit();
 
-        editPanelFragment.getEditor().text("handlers", "CONSOLE\nFILE");
-        editPanelFragment.getEditor().select("level", "INFO");
+        editor.text("handlers", "CONSOLE\nFILE");
+        editor.select("level", "INFO");
         boolean finished = editPanelFragment.save();
 
         assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "handlers", "[\"CONSOLE\",\"FILE\"]", 500);
-        verifier.verifyAttribute(address, "level", "INFO", 500);
+        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttribute("handlers", "[\"CONSOLE\",\"FILE\"]");
+        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttribute("level", "INFO");
     }
 }
