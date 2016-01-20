d7630dc909809f6acf282bdaf7aa6ef8f8b92a2f
Jan Kasik <jkasik@redhat.com>
2016-01-20 08:52:05 +0100

    Added backup/restore attributes command to RootLoggerTestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/logging/RootLoggerTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/logging/RootLoggerTestCase.java
@@ -1,35 +1,50 @@
 package org.jboss.hal.testsuite.test.configuration.logging;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.hal.testsuite.category.Standalone;
+import org.jboss.hal.testsuite.category.Shared;
 import org.jboss.hal.testsuite.creaper.ResourceVerifier;
+import org.jboss.hal.testsuite.creaper.command.BackupAndRestoreAttributes;
 import org.jboss.hal.testsuite.fragment.ConfigFragment;
 import org.jboss.hal.testsuite.fragment.formeditor.Editor;
 import org.jboss.hal.testsuite.page.config.LoggingPage;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.wildfly.extras.creaper.commands.logging.Logging;
+import org.wildfly.extras.creaper.core.CommandFailedException;
 import org.wildfly.extras.creaper.core.online.operations.Address;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by pcyprian on 25.8.15.
- */
 @RunWith(Arquillian.class)
-@Category(Standalone.class)
+@Category(Shared.class)
 public class RootLoggerTestCase extends LoggingAbstractTestCase {
-    //TODO add command for attribute backup and restore
+
     private static final Address ROOT_LOGGER_ADDRESS = LOGGING_SUBSYSTEM.and("root-logger", "ROOT");
+    private static BackupAndRestoreAttributes backup;
 
     @Drone
     private WebDriver browser;
     @Page
     private LoggingPage page;
+
+    @BeforeClass
+    public static void beforeClass() throws CommandFailedException {
+        backup = new BackupAndRestoreAttributes.Builder(ROOT_LOGGER_ADDRESS).build();
+        client.apply(backup.backup());
+    }
+
+    @AfterClass
+    public static void afterClass() throws CommandFailedException {
+        client.apply(backup.restore());
+    }
 
     @Before
     public void before() {
@@ -37,30 +52,35 @@ public class RootLoggerTestCase extends LoggingAbstractTestCase {
     }
 
     @Test
-    public void updateRootLoggerAttributes() throws Exception {
+    public void editLevel() throws Exception {
+        selectOptionAndVerify(ROOT_LOGGER_ADDRESS, "level", "WARN");
+    }
+
+    @Test
+    public void editFilterSpec() throws Exception {
+        editTextAndVerify(ROOT_LOGGER_ADDRESS, "filter-spec", "not(match(\"JBAS.*\"))");
+    }
+
+    @Test
+    public void editHandlers() throws Exception {
+        String handler = "testHandler_" + RandomStringUtils.randomAlphanumeric(5);
+        client.apply(Logging.handler()
+                .console()
+                .add(handler)
+                .build());
+        administration.reloadIfRequired();
+        editTextAreaAndVerify(ROOT_LOGGER_ADDRESS, "handlers", new String[]{handler});
+    }
+
+    @Test
+    public void undefineHandlers() throws Exception {
         ConfigFragment editPanelFragment = page.getConfigFragment();
         Editor editor = editPanelFragment.edit();
 
         editor.text("handlers", "");
-        editor.select("level", "WARN");
         boolean finished = editPanelFragment.save();
 
         assertTrue("Config should be saved and closed.", finished);
         new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttributeIsUndefined("handlers");
-        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttribute("level", "WARN");
-    }
-
-    @Test
-    public void setRootLoggerAttributesToDefault() throws Exception {
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-        Editor editor = editPanelFragment.edit();
-
-        editor.text("handlers", "CONSOLE\nFILE");
-        editor.select("level", "INFO");
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttribute("handlers", "[\"CONSOLE\",\"FILE\"]");
-        new ResourceVerifier(ROOT_LOGGER_ADDRESS, client).verifyAttribute("level", "INFO");
     }
 }
