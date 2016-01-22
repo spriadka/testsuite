d5023f2dfa533406d7df6e7c1f528f4c4c848a6f
Jan Kasik <jkasik@redhat.com>
2016-01-22 16:11:22 +0100

    Added Creaper to JournalTestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/providerSettings/JournalTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/providerSettings/JournalTestCase.java
@@ -1,5 +1,6 @@
 package org.jboss.hal.testsuite.test.configuration.messaging.providerSettings;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
@@ -11,6 +12,7 @@ import org.jboss.hal.testsuite.dmr.Dispatcher;
 import org.jboss.hal.testsuite.dmr.ResourceAddress;
 import org.jboss.hal.testsuite.dmr.ResourceVerifier;
 import org.jboss.hal.testsuite.page.config.MessagingPage;
+import org.jboss.hal.testsuite.test.configuration.messaging.AbstractMessagingTestCase;
 import org.jboss.hal.testsuite.util.ConfigUtils;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,39 +22,30 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.wildfly.extras.creaper.core.online.operations.Address;
+import org.wildfly.extras.creaper.core.online.operations.OperationException;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by pcyprian on 9.9.15.
- */
 @RunWith(Arquillian.class)
 @Category(Shared.class)
-public class JournalTestCase {
-    private static final String NAME = "test-provider";
-    private static final String ADD = "/subsystem=messaging-activemq/server=" + NAME + ":add()";
-    private static final String DOMAIN = "/profile=full-ha";
-    private static final String REMOVE = "/subsystem=messaging-activemq/server=" + NAME + ":remove";
+public class JournalTestCase extends AbstractMessagingTestCase {
+    private static final String SERVER_NAME = "test-provider_" + RandomStringUtils.randomAlphanumeric(5);
 
-    private String command;
-    private String removeCmd;
-    private ModelNode path = new ModelNode("/subsystem=messaging-activemq/server=" + NAME);
-    private ModelNode domainPath = new ModelNode("/profile=full-ha/subsystem=messaging-activemq/server=" + NAME);
-    private ResourceAddress address;
-    private static Dispatcher dispatcher;
-    private static ResourceVerifier verifier;
-    CliClient cliClient = CliClientFactory.getClient();
+    private static final Address SERVER_ADDRESS = MESSAGING_SUBSYSTEM.and("server", SERVER_NAME);
 
     @BeforeClass
-    public static void setUp() {
-        dispatcher = new Dispatcher();
-        verifier  = new ResourceVerifier(dispatcher);
+    public static void setUp() throws IOException {
+        operations.add(SERVER_ADDRESS);
     }
 
     @AfterClass
-    public static void tearDown() {
-        dispatcher.close();
+    public static void tearDown() throws IOException, OperationException {
+        operations.removeIfExists(SERVER_ADDRESS);
     }
 
     @Drone
@@ -62,182 +55,69 @@ public class JournalTestCase {
 
     @Before
     public void before() {
-        if (ConfigUtils.isDomain()) {
-            address = new ResourceAddress(domainPath);
-            command = DOMAIN + ADD;
-            removeCmd = DOMAIN + REMOVE;
-        } else {
-            address = new ResourceAddress(path);
-            command = ADD;
-            removeCmd = REMOVE;
-        }
-        cliClient.executeCommand(command);
+        page.selectProvider(SERVER_NAME);
+        page.InvokeProviderSettings();
+        page.switchToJournalTab();
     }
 
     @After
-    public void after() {
-        cliClient.executeCommand(removeCmd);
-        cliClient.reload();
+    public void after() throws InterruptedException, TimeoutException, IOException {
+        administration.reloadIfRequired();
     }
 
     @Test
-    public void updateJournalType() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().select("journal-type", "NIO");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-type", "NIO", 500);
+    public void updateJournalType() throws Exception {
+        selectOptionAndVerify(SERVER_ADDRESS, "journal-type", "NIO");
     }
 
     @Test
-    public void updateCreateJournalDir() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().checkbox("create-journal-dir", false);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "create-journal-dir", false, 500);
+    public void updateCreateJournalDir() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "create-journal-dir", false);
     }
 
     @Test
-    public void updateJournalSyncNonTransactional() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().checkbox("journal-sync-non-transactional", false);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-sync-non-transactional", false, 500);
+    public void updateJournalSyncNonTransactional() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "journal-sync-non-transactional", false);
     }
 
     @Test
-    public void updateJournalSyncTransactional() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().checkbox("journal-sync-transactional", false);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-sync-transactional", false, 500);
+    public void updateJournalSyncTransactional() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "journal-sync-transactional", false);
     }
 
     @Test
-    public void updateJournalMinFiles() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-min-files", "0");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertFalse("Config should not be saved and closed. 0 is invalid value.", finished);
-        verifier.verifyAttribute(address, "journal-min-files", "2", 500);
+    public void updateJournalMinFilesInvalid() throws Exception {
+        verifyIfErrorAppears("journal-min-files", "0");
     }
 
     @Test
     public void updateJournalMaxFilesWrongValue() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-max-io", "-1");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertFalse("Config should not be saved and closed.Invalid value.", finished);
-        verifier.verifyAttribute(address, "journal-max-io", "undefined", 500);
+        verifyIfErrorAppears("journal-max-io", "-1");
     }
 
     @Test
-    public void updateJournalBufferSize() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-buffer-size", "1024");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should not be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-buffer-size", "1024", 500);
-
-        page.getWindowFragment().edit();
-        page.getWindowFragment().getEditor().text("journal-buffer-size", "");
-        finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should not be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-buffer-size", "undefined", 500);
+    public void updateJournalBufferSize() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "journal-buffer-size", 1024L);
+        undefineAndVerify(SERVER_ADDRESS, "journal-buffer-size");
     }
 
     @Test
-    public void updateJournalBufferTimeout() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-buffer-timeout", "1");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should not be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-buffer-timeout", "1", 500);
+    public void updateJournalBufferTimeout() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "journal-buffer-timeout", 1L);
     }
 
     @Test
-    public void updateJournalCompactMinFiles() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-compact-min-files", "1");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should not be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-compact-min-files", "1", 500);
+    public void updateJournalCompactMinFiles() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "journal-compact-min-files", 1);
     }
 
     @Test
-    public void updateJournalCompactPercentage() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-compact-percentage", "110");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should not be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-compact-percentage", "110", 500);
+    public void updateJournalCompactPercentage() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "journal-compact-percentage", 110);
     }
 
     @Test
-    public void updateJournalFileSize() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.switchToJournalTab();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("journal-file-size", "110");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should not be saved and closed.", finished);
-        verifier.verifyAttribute(address, "journal-file-size", "110", 500);
+    public void updateJournalFileSize() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "journal-file-size", 110L);
     }
 }
