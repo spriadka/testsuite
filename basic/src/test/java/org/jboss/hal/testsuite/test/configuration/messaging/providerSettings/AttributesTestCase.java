e7883fd79a388eb3e59edb8eca8ed07a4284926d
Jan Kasik <jkasik@redhat.com>
2016-01-22 15:45:27 +0100

    Added Creaper to AttributesTestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/providerSettings/AttributesTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/providerSettings/AttributesTestCase.java
@@ -1,5 +1,6 @@
 package org.jboss.hal.testsuite.test.configuration.messaging.providerSettings;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
@@ -11,6 +12,7 @@ import org.jboss.hal.testsuite.dmr.Dispatcher;
 import org.jboss.hal.testsuite.dmr.ResourceAddress;
 import org.jboss.hal.testsuite.dmr.ResourceVerifier;
 import org.jboss.hal.testsuite.page.config.MessagingPage;
+import org.jboss.hal.testsuite.test.configuration.messaging.AbstractMessagingTestCase;
 import org.jboss.hal.testsuite.util.ConfigUtils;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,29 +22,22 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.wildfly.extras.creaper.core.online.operations.Address;
+import org.wildfly.extras.creaper.core.online.operations.OperationException;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by pcyprian on 9.9.15.
- */
 @RunWith(Arquillian.class)
 @Category(Shared.class)
-public class AttributesTestCase {
-    private static final String NAME = "test-provider";
-    private static final String ADD = "/subsystem=messaging-activemq/server=" + NAME + ":add()";
-    private static final String DOMAIN = "/profile=full-ha";
-    private static final String REMOVE = "/subsystem=messaging-activemq/server=" + NAME + ":remove";
+public class AttributesTestCase extends AbstractMessagingTestCase {
 
-    private String command;
-    private String removeCmd;
-    private ModelNode path = new ModelNode("/subsystem=messaging-activemq/server=" + NAME);
-    private ModelNode domainPath = new ModelNode("/profile=full-ha/subsystem=messaging-activemq/server=" + NAME);
-    private ResourceAddress address;
-    private static Dispatcher dispatcher;
-    private static ResourceVerifier verifier;
-    CliClient cliClient = CliClientFactory.getClient();
+    private static final String SERVER_NAME = "test-provider_" + RandomStringUtils.randomAlphanumeric(5);
+
+    private static final Address SERVER_ADDRESS = MESSAGING_SUBSYSTEM.and("server", SERVER_NAME);
 
     @Drone
     private WebDriver browser;
@@ -50,176 +45,78 @@ public class AttributesTestCase {
     private MessagingPage page;
 
     @BeforeClass
-    public static void setUp() {
-        dispatcher = new Dispatcher();
-        verifier = new ResourceVerifier(dispatcher);
+    public static void setUp() throws IOException {
+        operations.add(SERVER_ADDRESS);
     }
 
     @Before
     public void before() {
-        if (ConfigUtils.isDomain()) {
-            address = new ResourceAddress(domainPath);
-            command = DOMAIN + ADD;
-            removeCmd = DOMAIN + REMOVE;
-        } else {
-            address = new ResourceAddress(path);
-            command = ADD;
-            removeCmd = REMOVE;
-        }
-        cliClient.executeCommand(command);
+        page.selectProvider(SERVER_NAME);
+        page.InvokeProviderSettings();
     }
 
     @After
-    public void after() {
-        cliClient.executeCommand(removeCmd);
-        cliClient.reload();
+    public void after() throws InterruptedException, TimeoutException, IOException {
+        administration.reloadIfRequired();
     }
 
     @AfterClass
-    public static void tearDown() {
-        dispatcher.close();
+    public static void tearDown() throws IOException, OperationException {
+        operations.removeIfExists(SERVER_ADDRESS);
     }
 
     @Test
-    public void updateManagementAddress() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("management-address", "management");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "management-address", "management", 500);
+    public void updateManagementAddress() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "management-address", "management");
     }
 
     @Test
-    public void updateNotificationAddress() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("management-notification-address", "a.n");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "management-notification-address", "a.n", 500);
-    }
-
-    @Test //https://issues.jboss.org/browse/HAL-837
-    public void updateThreadPollMaxSize() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("thread-pool-max-size", "-1");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "thread-pool-max-size", "-1", 500);
+    public void updateNotificationAddress() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "management-notification-address", "a.n");
     }
 
     @Test
-    public void updateScheduledThreadPollMaxSize() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("scheduled-thread-pool-max-size", "10");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "scheduled-thread-pool-max-size", "10", 500);
+    public void updateThreadPollMaxSize() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "thread-pool-max-size", -1);
     }
 
     @Test
-    public void updateTransacitonTimeout() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("transaction-timeout", "1");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "transaction-timeout", "1", 500);
+    public void updateScheduledThreadPollMaxSize() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "scheduled-thread-pool-max-size", 10);
     }
 
     @Test
-    public void updateTransacitonTimeoutWrongValue() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("transaction-timeout", "-2");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertFalse("Config should not be saved and closed.Invalid value.", finished);
-        verifier.verifyAttribute(address, "transaction-timeout", "300000", 500);
+    public void updateTransactionTimeout() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "transaction-timeout", 1L);
     }
 
     @Test
-    public void updateTransacitonTimeoutScanPeriod() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().text("transaction-timeout-scan-period", "100000");
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "transaction-timeout-scan-period", "100000", 500);
+    public void updateTransactionTimeoutWrongValue() {
+        verifyIfErrorAppears("transaction-timeout", "-2");
     }
 
     @Test
-    public void updateStatisticsEnabled() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().checkbox("statistics-enabled", true);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "statistics-enabled", true, 500);
+    public void updateTransactionTimeoutScanPeriod() throws Exception {
+        editTextAndVerify(SERVER_ADDRESS, "transaction-timeout-scan-period", 100000L);
     }
 
     @Test
-    public void updateWildCardRoutingEnabled() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().checkbox("wild-card-routing-enabled", false);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "wild-card-routing-enabled", false, 500);
+    public void updateStatisticsEnabled() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "statistics-enabled", true);
     }
 
     @Test
-    public void updatePersistenceEnabled() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
-
-        page.getWindowFragment().getEditor().checkbox("persistence-enabled", false);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "persistence-enabled", false, 500);
+    public void updateWildCardRoutingEnabled() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "wild-card-routing-enabled", false);
     }
 
     @Test
-    public void updatePersistIdCache() {
-        page.selectProvider(NAME);
-        page.InvokeProviderSettings();
-        page.getWindowFragment().edit();
+    public void updatePersistenceEnabled() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "persistence-enabled", false);
+    }
 
-        page.getWindowFragment().getEditor().checkbox("persist-id-cache", false);
-        boolean finished =  page.getWindowFragment().save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "persist-id-cache", false, 500);
+    @Test
+    public void updatePersistIdCache() throws Exception {
+        editCheckboxAndVerify(SERVER_ADDRESS, "persist-id-cache", false);
     }
 }
