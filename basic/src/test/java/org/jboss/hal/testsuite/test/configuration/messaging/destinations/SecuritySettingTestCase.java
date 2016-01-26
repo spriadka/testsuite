f897911a9b076b49a08b9f8ad86ded1cf877bd3b
Jan Kasik <jkasik@redhat.com>
2016-01-26 14:01:14 +0100

    Fixed and added Creaper to SecuritySettingTestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/destinations/SecuritySettingTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/destinations/SecuritySettingTestCase.java
@@ -1,18 +1,13 @@
 package org.jboss.hal.testsuite.test.configuration.messaging.destinations;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.hal.testsuite.creaper.ResourceVerifier;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.dmr.ModelNode;
 import org.jboss.hal.testsuite.category.Shared;
-import org.jboss.hal.testsuite.cli.CliClient;
-import org.jboss.hal.testsuite.cli.CliClientFactory;
-import org.jboss.hal.testsuite.dmr.Dispatcher;
-import org.jboss.hal.testsuite.dmr.ResourceAddress;
-import org.jboss.hal.testsuite.dmr.ResourceVerifier;
-import org.jboss.hal.testsuite.fragment.ConfigFragment;
 import org.jboss.hal.testsuite.page.config.MessagingPage;
-import org.jboss.hal.testsuite.util.ConfigUtils;
+import org.jboss.hal.testsuite.test.configuration.messaging.AbstractMessagingTestCase;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -21,39 +16,42 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.wildfly.extras.creaper.core.online.operations.Address;
+import org.wildfly.extras.creaper.core.online.operations.Batch;
+import org.wildfly.extras.creaper.core.online.operations.OperationException;
 
-import static org.junit.Assert.assertTrue;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
-/**
- * Created by pcyprian on 8.9.15.
- */
 @RunWith(Arquillian.class)
 @Category(Shared.class)
-public class SecuritySettingTestCase {
-    private static final String PATTERN = "test-p";
-    private static final String ROLE = "role";
-    private static final String ADD = "/subsystem=messaging-activemq/server==default/security-setting=" + PATTERN + ":add()";
-    private static final String DOMAIN = "/profile=full-ha";
+public class SecuritySettingTestCase extends AbstractMessagingTestCase {
 
-    private String command;
-    private String addRole = "/subsystem=messaging-activemq/server==default/security-setting=" + PATTERN + "/role=" + ROLE + ":add()";
-    private String remove = "/subsystem=messaging-activemq/server=default/security-setting=" + PATTERN + ":remove";
-    private ModelNode path = new ModelNode("/subsystem=messaging-activemq/server=default/security-setting=" + PATTERN + "/role=" + ROLE );
-    private ModelNode domainPath = new ModelNode("/profile=full-ha/subsystem=messaging-activemq/server=default/security-setting=" + PATTERN + "/role=" + ROLE );
-    private ResourceAddress address;
-    private static Dispatcher dispatcher;
-    private static ResourceVerifier verifier;
-    CliClient cliClient = CliClientFactory.getClient();
+    private static final String ROLE = "myrole_" + RandomStringUtils.randomAlphanumeric(3);
+
+    private static final String PATTERN = "test-pattern_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String PATTERN_TBA = "test-pattern-TBA_" + RandomStringUtils.randomAlphanumeric(5);
+    private static final String PATTERN_TBR = "test-pattern-TBR_" + RandomStringUtils.randomAlphanumeric(5);
+
+    private static final Address PATTERN_ADDRESS = DEFAULT_MESSAGING_SERVER.and("security-setting", PATTERN)
+            .and("role", ROLE);
+    private static final Address PATTERN_TBA_ADDRESS = DEFAULT_MESSAGING_SERVER.and("security-setting", PATTERN_TBA)
+            .and("role", ROLE);
+    private static final Address PATTERN_TBR_ADDRESS = DEFAULT_MESSAGING_SERVER.and("security-setting", PATTERN_TBR)
+            .and("role", ROLE);
 
     @BeforeClass
-    public static void setUp() {
-        dispatcher = new Dispatcher();
-        verifier  = new ResourceVerifier(dispatcher);
+    public static void setUp() throws InterruptedException, TimeoutException, IOException {
+        addSecuritySettings(PATTERN, ROLE);
+        addSecuritySettings(PATTERN_TBR, ROLE);
+        administration.reloadIfRequired();
     }
 
     @AfterClass
-    public static void tearDown() {
-        dispatcher.close();
+    public static void tearDown() throws IOException, OperationException {
+        operations.removeIfExists(PATTERN_ADDRESS);
+        operations.removeIfExists(PATTERN_TBA_ADDRESS);
+        operations.removeIfExists(PATTERN_TBR_ADDRESS);
     }
 
     @Drone
@@ -63,212 +61,76 @@ public class SecuritySettingTestCase {
 
     @Before
     public void before() {
-        if (ConfigUtils.isDomain()) {
-            address = new ResourceAddress(domainPath);
-            command = DOMAIN + ADD;
-            remove = DOMAIN + remove;
-            addRole = DOMAIN + addRole;
-        } else {
-            address = new ResourceAddress(path);
-            command = ADD;
-        }
+        page.navigateToMessaging();
+        page.selectQueuesAndTopics();
+        page.switchToSecuritySettings();
+        page.selectInTable(PATTERN);
     }
 
     @After
-    public void after() {
-        cliClient.executeCommand(remove);
+    public void after() throws InterruptedException, TimeoutException, IOException {
+        administration.reloadIfRequired();
     }
 
     @Test
-    public void addSecuritySetting() {
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.addSecuritySettings(PATTERN, ROLE);
-
-        verifier.verifyResource(address, true);
-
-        cliClient.executeCommand(remove);
-
-        verifier.verifyResource(address, false);
+    public void addSecuritySetting() throws Exception {
+        page.addSecuritySettings(PATTERN_TBA, ROLE);
+        new ResourceVerifier(PATTERN_TBA_ADDRESS, client).verifyExists();
     }
 
     @Test
-    public void updateSendSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
-        page.edit();
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("send", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "send", true, 500);
-
-        cliClient.executeCommand(remove);
+    public void updateSendSecuritySetting() throws Exception {
+        editCheckboxAndVerify(PATTERN_ADDRESS, "send", true);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "send", false);
    }
 
     @Test
-         public void updateConsumeSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
-        page.edit();
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("consume", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "consume", true, 500);
-
-        cliClient.executeCommand(remove);
+    public void updateConsumeSecuritySetting() throws Exception {
+        editCheckboxAndVerify(PATTERN_ADDRESS, "consume", true);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "consume", false);
     }
 
     @Test
-    public void updateManageSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
-        page.edit();
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("manage", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "manage", true, 500);
-
-        cliClient.executeCommand(remove);
+    public void updateManageSecuritySetting() throws Exception {
+        editCheckboxAndVerify(PATTERN_ADDRESS, "manage", true);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "manage", false);
     }
 
     @Test
-    public void updateCreateDurableSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
+    public void updateCreateDurableSecuritySetting() throws Exception {
         page.clickAdvanced();
-        page.edit();
-
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("createDurableQueue", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "create-durable-queue", true, 500);
-
-        cliClient.executeCommand(remove);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "createDurableQueue", "create-durable-queue", true);
     }
 
     @Test
-    public void updateDeleteDurableSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
+    public void updateDeleteDurableSecuritySetting() throws Exception {
         page.clickAdvanced();
-        page.edit();
-
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("deleteDurableQueue", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "delete-durable-queue", true, 500);
-
-        cliClient.executeCommand(remove);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "deleteDurableQueue", "delete-durable-queue", true);
     }
 
     @Test
-    public void updateCreateNonDurableSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
+    public void updateCreateNonDurableSecuritySetting() throws Exception {
         page.clickAdvanced();
-        page.edit();
-
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("createNonDurableQueue", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "create-non-durable-queue", true, 500);
-
-        cliClient.executeCommand(remove);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "createNonDurableQueue", "create-non-durable-queue", true);
     }
 
     @Test
-    public void updateDeleteNonDurableSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
-
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        page.selectInTable(PATTERN, 0);
+    public void updateDeleteNonDurableSecuritySetting() throws Exception {
         page.clickAdvanced();
-        page.edit();
-
-        ConfigFragment editPanelFragment = page.getConfigFragment();
-
-        editPanelFragment.getEditor().checkbox("deleteNonDurableQueue", true);
-        boolean finished = editPanelFragment.save();
-
-        assertTrue("Config should be saved and closed.", finished);
-        verifier.verifyAttribute(address, "delete-non-durable-queue", true, 500);
-
-        cliClient.executeCommand(remove);
+        editCheckboxAndVerify(PATTERN_ADDRESS, "deleteNonDurableQueue", "delete-non-durable-queue", true);
     }
 
     @Test
-    public void removeSecuritySetting() {
-        cliClient.executeCommand(command);
-        cliClient.executeCommand(addRole);
+    public void removeSecuritySetting() throws Exception {
+        page.remove(PATTERN_TBR);
+        new ResourceVerifier(PATTERN_TBR_ADDRESS, client).verifyDoesNotExist();
+    }
 
-        page.navigateToMessaging();
-        page.selectQueuesAndTopics();
-        page.switchToSecuritySettings();
-
-        verifier.verifyResource(address, true);
-        page.selectInTable(PATTERN, 0);
-        page.remove();
-
-        verifier.verifyResource(address, false);
+    private static void addSecuritySettings(String pattern, String role) throws IOException {
+        Batch batch = new Batch();
+        Address address = DEFAULT_MESSAGING_SERVER.and("security-setting", pattern);
+        batch.add(address);
+        batch.add(address.and("role", role));
+        operations.batch(batch);
     }
 }
