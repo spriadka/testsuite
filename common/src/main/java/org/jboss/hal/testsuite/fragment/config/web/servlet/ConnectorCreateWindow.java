9eec00dc7127e89a598ca394f6ad7a07eebc5e24
Pavel Jelinek <pjelinek@redhat.com>
2016-01-27 13:28:06 +0100

    Removing obsolete ConnectorCreateWindow.save() method.

--- common/src/main/java/org/jboss/hal/testsuite/fragment/config/web/servlet/ConnectorCreateWindow.java
+++ common/src/main/java/org/jboss/hal/testsuite/fragment/config/web/servlet/ConnectorCreateWindow.java
@@ -1,10 +1,6 @@
 package org.jboss.hal.testsuite.fragment.config.web.servlet;
 
 import org.jboss.hal.testsuite.fragment.shared.modal.WizardWindow;
-import org.jboss.hal.testsuite.util.PropUtils;
-import org.openqa.selenium.By;
-
-import static org.jboss.arquillian.graphene.Graphene.waitGui;
 
 /**
  * Created by mkrajcov on 3/26/15.
@@ -16,13 +12,6 @@ public class ConnectorCreateWindow extends WizardWindow {
     private static final String PROTOCOL = "protocol";
     private static final String SCHEME = "scheme";
     private static final String ENABLED = "enabled";
-
-    public void save() {
-        clickButton(PropUtils.get("modals.window.save.label"));
-        if (isClosed()) {
-            waitGui().until().element(By.className(PropUtils.get("resourcetable.cell.class"))).is().visible();
-        }
-    }
 
     public ConnectorCreateWindow name(String value) {
         getEditor().text(NAME, value);
