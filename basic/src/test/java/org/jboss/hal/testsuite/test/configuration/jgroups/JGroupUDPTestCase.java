0733bdbc89146490afe2f6039d9ed6a0dfc5a675
Jan Kasik <jkasik@redhat.com>
2016-01-27 12:53:00 +0100

    Fixed cases when reload required windows failed tests and added Creaper to JGroups tests

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/jgroups/JGroupUDPTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/jgroups/JGroupUDPTestCase.java
@@ -1,32 +1,30 @@
 package org.jboss.hal.testsuite.test.configuration.jgroups;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.hal.testsuite.category.Shared;
-import org.jboss.hal.testsuite.util.ConfigUtils;
 import org.jboss.hal.testsuite.util.Console;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.wildfly.extras.creaper.core.online.operations.Address;
 
-/**
- * Created by jkasik <jkasik@redhat.com>
- */
+import java.io.IOException;
+
 @RunWith(Arquillian.class)
 @Category(Shared.class)
 public class JGroupUDPTestCase extends JGroupAbstractTestCase {
 
     @BeforeClass
-    public static void setUpUDP() {
-        String profile = "";
-        if (ConfigUtils.isDomain()) {
-            profile = "/profile=full-ha";
-        }
-        verifier.setDmrPath(profile + "/subsystem=jgroups/stack=udp/transport=UDP");
-        jGroupsOperations.setBaseDrmPath(profile + "/subsystem=jgroups/stack=udp/");
-        jGroupsOperations.setTransport("UDP");
-        jGroupsOperations.addTransportProperty(PROPERTY_NAME_P, PROPERTY_VALUE_P);
-        jGroupsOperations.addProtocolProperty(DEFAULT_PROTOCOL, PROPERTY_NAME_P, PROPERTY_VALUE_P);
+    public static void setUpUDP() throws IOException {
+        BASE_ADDRESS =  Address.subsystem("jgroups").and("stack", "udp");
+        PROTOCOL_ADDRESS = BASE_ADDRESS.and("protocol", DEFAULT_PROTOCOL);
+        TRANSPORT_ADDRESS = BASE_ADDRESS.and("transport", "UDP");
+
+        jGroupsOperations.addProperty(TRANSPORT_ADDRESS, TRANSPORT_PROPERTY_TBR, RandomStringUtils.randomAlphanumeric(5));
+        jGroupsOperations.addProperty(PROTOCOL_ADDRESS, PROTOCOL_PROPERTY_TBR, RandomStringUtils.randomAlphanumeric(5));
+
     }
 
     @Before
--- common/src/main/java/org/jboss/hal/testsuite/fragment/config/jgroups/JGroupsProtocolPropertiesFragment.java
+++ common/src/main/java/org/jboss/hal/testsuite/fragment/config/jgroups/JGroupsProtocolPropertiesFragment.java
@@ -1,14 +1,18 @@
 package org.jboss.hal.testsuite.fragment.config.jgroups;
 
 import org.jboss.hal.testsuite.fragment.config.resourceadapters.ConfigPropertiesFragment;
+import org.jboss.hal.testsuite.fragment.shared.modal.ConfirmationWindow;
 
-/**
- * @author Jan Kasik <jkasik@redhat.com>
- *         Created on 9.10.15.
- */
 public class JGroupsProtocolPropertiesFragment extends ConfigPropertiesFragment {
 
+    @Override
     public JGroupsProtocolPropertyWizard addProperty() {
         return getResourceManager().addResource(JGroupsProtocolPropertyWizard.class);
+    }
+
+    @Override
+    public void removeProperty(String name) {
+        ConfirmationWindow confirmationWindow = getResourceManager().removeResource(name);
+        confirmationWindow.confirmAndDismissReloadRequiredMessage();
     }
 }
