38023a8e459c1843525158ace115d8fe6756f106
Jan Kasik <jkasik@redhat.com>
2016-02-17 16:42:51 +0100

    Removed unused method in ResourceAdaptersPage

--- common/src/main/java/org/jboss/hal/testsuite/page/config/ResourceAdaptersPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/ResourceAdaptersPage.java
@@ -4,7 +4,6 @@ import org.jboss.arquillian.graphene.page.Location;
 import org.jboss.hal.testsuite.finder.Application;
 import org.jboss.hal.testsuite.finder.Column;
 import org.jboss.hal.testsuite.finder.FinderNames;
-import org.jboss.hal.testsuite.finder.FinderNavigation;
 import org.jboss.hal.testsuite.finder.Row;
 import org.jboss.hal.testsuite.fragment.ConfigAreaFragment;
 import org.jboss.hal.testsuite.fragment.ConfigFragment;
@@ -12,15 +11,13 @@ import org.jboss.hal.testsuite.fragment.WindowState;
 import org.jboss.hal.testsuite.fragment.config.resourceadapters.ResourceAdapterWizard;
 import org.jboss.hal.testsuite.fragment.config.resourceadapters.ResourceAdaptersConfigArea;
 import org.jboss.hal.testsuite.fragment.shared.modal.ConfirmationWindow;
-import org.jboss.hal.testsuite.page.Navigatable;
-import org.jboss.hal.testsuite.util.ConfigUtils;
 import org.jboss.hal.testsuite.util.Console;
 
 /**
  * @author mkrajcov <mkrajcov@redhat.com>
  */
 @Location("#profile")
-public class ResourceAdaptersPage extends ConfigurationPage implements Navigatable {
+public class ResourceAdaptersPage extends ConfigurationPage {
 
     private static final String
         RA_SUBSYSTEM_LABEL = "Resource Adapters",
@@ -54,22 +51,5 @@ public class ResourceAdaptersPage extends ConfigurationPage implements Navigatab
 
     public ConfigFragment switchConfigAreaTabTo(String tabLabel) {
         return getConfig(ConfigAreaFragment.class).switchTo(tabLabel);
-    }
-
-    public void navigate() {
-        FinderNavigation navigation;
-        if (ConfigUtils.isDomain()) {
-            navigation = new FinderNavigation(browser, DomainConfigEntryPoint.class)
-                    .addAddress(FinderNames.CONFIGURATION, FinderNames.PROFILES)
-                    .addAddress(FinderNames.PROFILE, ConfigUtils.getDefaultProfile())
-                    .addAddress(FinderNames.SUBSYSTEM, "Resource Adapters");
-
-        } else {
-            navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
-                    .addAddress(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
-                    .addAddress(FinderNames.SUBSYSTEM, "Resource Adapters");
-        }
-        navigation.selectColumn();
-        Console.withBrowser(browser).dismissReloadRequiredWindowIfPresent();
     }
 }
