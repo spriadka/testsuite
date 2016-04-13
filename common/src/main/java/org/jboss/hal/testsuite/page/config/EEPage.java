51c46480058b0d121a360a82c90d21a5f6da99c4
Jan Kasik <jkasik@redhat.com>
2016-04-13 13:10:14 +0200

    Fixed EETestCase, added Creaper

--- common/src/main/java/org/jboss/hal/testsuite/page/config/EEPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/EEPage.java
@@ -1,11 +1,31 @@
 package org.jboss.hal.testsuite.page.config;
 
 import org.jboss.arquillian.graphene.page.Location;
-import org.jboss.hal.testsuite.page.ConfigPage;
+import org.jboss.hal.testsuite.finder.Application;
+import org.jboss.hal.testsuite.finder.FinderNames;
+import org.jboss.hal.testsuite.finder.FinderNavigation;
+import org.jboss.hal.testsuite.page.Navigatable;
+import org.jboss.hal.testsuite.util.ConfigUtils;
 
 /**
  * @author mkrajcov <mkrajcov@redhat.com>
  */
 @Location("#ee")
-public class EEPage extends ConfigPage {
+public class EEPage extends ConfigurationPage implements Navigatable {
+
+    public void navigate() {
+        FinderNavigation navigation;
+        if (ConfigUtils.isDomain()) {
+            navigation = new FinderNavigation(browser, DomainConfigEntryPoint.class);
+            navigation.step(FinderNames.CONFIGURATION, FinderNames.PROFILES)
+                    .step(FinderNames.PROFILE, ConfigUtils.getDefaultProfile())
+                    .step(FinderNames.SUBSYSTEM, "EE");
+        } else {
+            navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class);
+            navigation.step(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
+                    .step(FinderNames.SUBSYSTEM, "EE");
+        }
+        navigation.selectRow().invoke(FinderNames.VIEW);
+        Application.waitUntilVisible();
+    }
 }
