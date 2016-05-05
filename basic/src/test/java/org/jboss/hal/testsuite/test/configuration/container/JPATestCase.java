8f9296095ee8e1f1e04c82711e7cd6d615f1d52c
Jan Kasik <jkasik@redhat.com>
2016-05-05 13:34:24 +0200

    Added Creaper and improved cleanup in JPATestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/container/JPATestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/container/JPATestCase.java
@@ -4,21 +4,28 @@ import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.hal.testsuite.category.Standalone;
-import org.jboss.hal.testsuite.cli.CliClient;
-import org.jboss.hal.testsuite.cli.CliClientFactory;
-import org.jboss.hal.testsuite.cli.CliConstants;
+import org.jboss.hal.testsuite.creaper.ManagementClientProvider;
 import org.jboss.hal.testsuite.finder.Application;
 import org.jboss.hal.testsuite.finder.FinderNames;
 import org.jboss.hal.testsuite.finder.FinderNavigation;
 import org.jboss.hal.testsuite.page.config.JPAPage;
 import org.jboss.hal.testsuite.page.config.StandaloneConfigEntryPoint;
-import org.jboss.hal.testsuite.test.util.ConfigAreaChecker;
-import org.jboss.hal.testsuite.util.ResourceVerifier;
+import org.jboss.hal.testsuite.util.ConfigChecker;
+import org.jboss.hal.testsuite.util.Console;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.wildfly.extras.creaper.core.online.ModelNodeResult;
+import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
+import org.wildfly.extras.creaper.core.online.operations.Address;
+import org.wildfly.extras.creaper.core.online.operations.Operations;
+import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author mkrajcov <mkrajcov@redhat.com>
@@ -27,11 +34,14 @@ import org.openqa.selenium.WebDriver;
 @Category(Standalone.class)
 public class JPATestCase {
 
-    private CliClient client = CliClientFactory.getClient();
-    private ResourceVerifier verifier = new ResourceVerifier(CliConstants.JPA_SUBSYSTEM_ADDRESS, client);
-    private ConfigAreaChecker checker = new ConfigAreaChecker(verifier);
+    private static final OnlineManagementClient client = ManagementClientProvider.createOnlineManagementClient();
+    private static final Administration administration = new Administration(client);
+    private final Operations operations = new Operations(client);
 
-    private FinderNavigation navigation;
+    private static final String DEFAULT_DATASOURCE = "default-datasource";
+    private static final String DEFAULT_PERSISTENCE_INHERITANCE = "default-extended-persistence-inheritance";
+
+    private final Address jpaAddress = Address.subsystem("jpa");
 
     @Drone
     public WebDriver browser;
@@ -41,7 +51,7 @@ public class JPATestCase {
 
     @Before
     public void before() {
-        navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
+        FinderNavigation navigation = new FinderNavigation(browser, StandaloneConfigEntryPoint.class)
                 .step(FinderNames.CONFIGURATION, FinderNames.SUBSYSTEMS)
                 .step(FinderNames.SUBSYSTEM, "JPA");
 
@@ -49,15 +59,50 @@ public class JPATestCase {
         Application.waitUntilVisible();
     }
 
-    @Test
-    public void editDefaultDataSource() {
-        checker.editTextAndAssert(page, "defaultDataSource", "ds").dmrAttribute("default-datasource").invoke();
+    @AfterClass
+    public static void afterClass() throws IOException, TimeoutException, InterruptedException {
+        try {
+            administration.restartIfRequired();
+            administration.reloadIfRequired();
+        } finally {
+            client.close();
+        }
     }
 
     @Test
-    public void editPersistenceInheritance() {
-        checker.editSelectAndAssert(page, "inheritance", "SHALLOW").dmrAttribute("default-extended-persistence-inheritance").invoke();
-        checker.editSelectAndAssert(page, "inheritance", "DEEP").dmrAttribute("default-extended-persistence-inheritance").invoke();
+    public void editDefaultDataSource() throws Exception {
+        ModelNodeResult value = operations.readAttribute(jpaAddress, DEFAULT_DATASOURCE);
+        try {
+            new ConfigChecker.Builder(client, jpaAddress)
+                    .configFragment(page.getConfigFragment())
+                    .editAndSave(ConfigChecker.InputType.TEXT, "defaultDataSource", "ExampleDS")
+                    .verifyFormSaved()
+                    .verifyAttribute(DEFAULT_DATASOURCE, "ExampleDS");
+        } finally {
+            operations.writeAttribute(jpaAddress, DEFAULT_DATASOURCE, value.value());
+        }
+    }
+
+    @Test
+    public void editPersistenceInheritance() throws Exception {
+        ModelNodeResult value = operations.readAttribute(jpaAddress, DEFAULT_PERSISTENCE_INHERITANCE);
+        try {
+            new ConfigChecker.Builder(client, jpaAddress)
+                    .configFragment(page.getConfigFragment())
+                    .editAndSave(ConfigChecker.InputType.SELECT, "inheritance", "SHALLOW")
+                    .verifyFormSaved()
+                    .verifyAttribute(DEFAULT_PERSISTENCE_INHERITANCE, "SHALLOW");
+
+            Console.withBrowser(browser).dismissReloadRequiredWindowIfPresent();
+
+            new ConfigChecker.Builder(client, jpaAddress)
+                    .configFragment(page.getConfigFragment())
+                    .editAndSave(ConfigChecker.InputType.SELECT, "inheritance", "DEEP")
+                    .verifyFormSaved()
+                    .verifyAttribute(DEFAULT_PERSISTENCE_INHERITANCE, "DEEP");
+        } finally {
+            operations.writeAttribute(jpaAddress, DEFAULT_PERSISTENCE_INHERITANCE, value.value());
+        }
     }
 
 }
