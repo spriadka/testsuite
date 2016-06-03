6709a65fe1a9f85619f79972d11c820f8a89ca28
Pavel Jelinek <pjelinek@redhat.com>
2016-06-03 11:46:38 +0200

    JBEAP-4467 Ignore ConnectorServicesTestCase
    This feature org.apache.activemq.artemis.core.server.ConnectorServiceFactory to be manually added to server.
    This feature is at least not widely used.
    This feature is proposed to be removed from console by https://issues.jboss.org/browse/JBEAP-4467.

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/connections/ConnectorServicesTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/messaging/connections/ConnectorServicesTestCase.java
@@ -12,6 +12,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -24,11 +25,13 @@ import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertTrue;
 
+@Ignore("JBEAP-4467")
 @RunWith(Arquillian.class)
 @Category(Shared.class)
 public class ConnectorServicesTestCase extends AbstractMessagingTestCase {
     /* There should be no reload performed in this test case because it is error prone and possibly causes crash of
-     * ActiveMQ. See note below.*/
+     * ActiveMQ. See note below.
+     * After validation of factory-class value added and due to https://issues.jboss.org/browse/JBEAP-4467 this will be ignored.*/
 
     private static final String CONNECTOR_SERVICE = "connector-service_" + RandomStringUtils.randomAlphanumeric(5);
     private static final String CONNECTOR_SERVICE_TBA = "connector-service-TBA_" + RandomStringUtils.randomAlphanumeric(5);
@@ -46,7 +49,7 @@ public class ConnectorServicesTestCase extends AbstractMessagingTestCase {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        operations.add(CONNECTOR_SERVICE_ADDRESS, Values.of("factory-class", FACTORY_CLASS));
+        operations.add(CONNECTOR_SERVICE_ADDRESS, Values.of("factory-class", FACTORY_CLASS)).assertSuccess();
         PropertiesOps.addProperty(CONNECTOR_SERVICE_ADDRESS, client, PROPERTY_TBR_KEY, "test");
         new ResourceVerifier(CONNECTOR_SERVICE_ADDRESS, client).verifyExists();
         operations.add(CONNECTOR_SERVICE_ADDRESS_TBR, Values.of("factory-class", FACTORY_CLASS));
