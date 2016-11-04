ff90f29b9fcdcebde164ba853c37540b732d2bce
Jan Kasik <jkasik@redhat.com>
2016-11-04 12:10:43 +0100

    Fixed SecurityManagerTestCase

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/securitymanager/SecurityManagerTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/securitymanager/SecurityManagerTestCase.java
@@ -3,7 +3,6 @@ package org.jboss.hal.testsuite.test.configuration.securitymanager;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
 import org.jboss.hal.testsuite.category.Shared;
 import org.jboss.hal.testsuite.creaper.ManagementClientProvider;
 import org.jboss.hal.testsuite.creaper.ResourceVerifier;
@@ -12,7 +11,6 @@ import org.jboss.hal.testsuite.fragment.shared.modal.WizardWindowWithAdvancedSel
 import org.jboss.hal.testsuite.page.config.SecurityManagerPage;
 import org.jboss.hal.testsuite.util.Console;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -32,6 +30,8 @@ import java.util.concurrent.TimeoutException;
 @Category(Shared.class)
 public class SecurityManagerTestCase {
 
+    //TODO expand tests for deployment permission attributes when https://issues.jboss.org/browse/HAL-1225 is resolved
+
     @Drone
     private WebDriver browser;
 
@@ -47,9 +47,9 @@ public class SecurityManagerTestCase {
             SECURITY_MANAGER = "security-manager",
             DEPLOYMENT_PERMISSIONS = "deployment-permissions";
 
-    private static final Address DEFAULT_PERMISSIONS_ADDRESS = Address.subsystem(SECURITY_MANAGER).and(DEPLOYMENT_PERMISSIONS, DEFAULT);
+    private static final Address DEFAULT_PERMISSIONS_INSTANCE_ADDRESS = Address.subsystem(SECURITY_MANAGER).and(DEPLOYMENT_PERMISSIONS, DEFAULT);
 
-    private static BackupAndRestoreAttributes backup = new BackupAndRestoreAttributes.Builder(DEFAULT_PERMISSIONS_ADDRESS).build();
+    private static BackupAndRestoreAttributes backup = new BackupAndRestoreAttributes.Builder(DEFAULT_PERMISSIONS_INSTANCE_ADDRESS).build();
 
 
     @BeforeClass
@@ -60,9 +60,7 @@ public class SecurityManagerTestCase {
     @AfterClass
     public static void afterClass() throws IOException, OperationException, CommandFailedException, TimeoutException, InterruptedException {
         try {
-            if (!operations.exists(DEFAULT_PERMISSIONS_ADDRESS)) {
-                operations.add(DEFAULT_PERMISSIONS_ADDRESS);
-            }
+            addDefaultInstanceInModelIfNotPresent();
             client.apply(backup.restore());
             administration.reloadIfRequired();
         } finally {
@@ -70,14 +68,14 @@ public class SecurityManagerTestCase {
         }
     }
 
-    @Before
-    public void before() {
-        page.navigate();
-    }
-
-    @InSequence(0)
     @Test
-    public void removeDefaultInstance() throws Exception {
+    public void removeDefaultInstanceOfDeploymentPermissions() throws Exception {
+        addDefaultInstanceInModelIfNotPresent();
+
+        page.navigate();
+
+        Console.withBrowser(browser).dismissReloadRequiredWindowIfPresent();
+
         page.treeNavigation()
                 .step(DEPLOYMENT_PERMISSIONS)
                 .navigateToTreeItem()
@@ -87,13 +85,15 @@ public class SecurityManagerTestCase {
                 .removeResource(DEFAULT)
                 .confirmAndDismissReloadRequiredMessage();
 
-        new ResourceVerifier(DEFAULT_PERMISSIONS_ADDRESS, client)
-                .verifyDoesNotExist();
+        new ResourceVerifier(DEFAULT_PERMISSIONS_INSTANCE_ADDRESS, client).verifyDoesNotExist();
     }
 
-    @InSequence(1)
     @Test
-    public void addDefaultInstance() throws Exception {
+    public void addDefaultInstanceOfDeploymentPermissions() throws Exception {
+        removeDefaultInstanceInModel();
+
+        page.navigate();
+
         Console.withBrowser(browser).dismissReloadRequiredWindowIfPresent();
 
         page.treeNavigation()
@@ -108,9 +108,21 @@ public class SecurityManagerTestCase {
                 .clickContinue()
                 .finish();
 
-        new ResourceVerifier(DEFAULT_PERMISSIONS_ADDRESS, client)
-                .verifyExists();
+        new ResourceVerifier(DEFAULT_PERMISSIONS_INSTANCE_ADDRESS, client).verifyExists();
     }
 
+    private static void removeDefaultInstanceInModel() throws IOException {
+        operations.remove(DEFAULT_PERMISSIONS_INSTANCE_ADDRESS).assertSuccess();
+    }
+
+    private static void addDefaultInstanceInModel() throws IOException {
+        operations.add(DEFAULT_PERMISSIONS_INSTANCE_ADDRESS).assertSuccess();
+    }
+
+    private static void addDefaultInstanceInModelIfNotPresent() throws IOException, OperationException {
+        if (!operations.exists(DEFAULT_PERMISSIONS_INSTANCE_ADDRESS)) {
+            addDefaultInstanceInModel();
+        }
+    }
 
 }
