44c080786fb6aa4b35f9737ba94d305c0eba769e
Pavel Jelinek <pjelinek@redhat.com>
2017-03-10 12:44:02 +0100

    Ensure correct initialization of TransactionsMetricsPage

--- common/src/main/java/org/jboss/hal/testsuite/page/runtime/TransactionsMetricsPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/runtime/TransactionsMetricsPage.java
@@ -1,13 +1,11 @@
 package org.jboss.hal.testsuite.page.runtime;
 
-import org.jboss.arquillian.graphene.page.Location;
 import org.jboss.hal.testsuite.fragment.MetricsAreaFragment;
 import org.jboss.hal.testsuite.page.MetricsPage;
 
 /**
  * @author mkrajcov <mkrajcov@redhat.com>
  */
-@Location("#tx-metrics")
 public class TransactionsMetricsPage extends MetricsPage {
 
     public void navigate() {
