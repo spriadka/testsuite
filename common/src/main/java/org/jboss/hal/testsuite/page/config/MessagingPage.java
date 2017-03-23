a8083a037dbbb00dd202f6b2c21787fb67a9c94f
Jan Kasik <jkasik@redhat.com>
2017-03-23 14:37:30 +0100

    Added state to WizardWindow, updated DiscoveryGroupTestCase

--- common/src/main/java/org/jboss/hal/testsuite/page/config/MessagingPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/MessagingPage.java
@@ -9,6 +9,7 @@ import org.jboss.hal.testsuite.fragment.ConfigFragment;
 import org.jboss.hal.testsuite.fragment.WindowFragment;
 import org.jboss.hal.testsuite.fragment.config.messaging.AddBridgeWizard;
 import org.jboss.hal.testsuite.fragment.config.messaging.AddClusterConnectionWizard;
+import org.jboss.hal.testsuite.fragment.config.messaging.AddDiscoveryGroupWizard;
 import org.jboss.hal.testsuite.fragment.config.messaging.AddMessagingProviderWindow;
 import org.jboss.hal.testsuite.fragment.config.messaging.MessagingConfigArea;
 import org.jboss.hal.testsuite.fragment.config.messaging.ProviderSettingsWindow;
@@ -205,11 +206,8 @@ public class MessagingPage extends ConfigPage implements Navigatable {
         addRemoteAcceptor(name, socketBinding);
     }
 
-    public void addDiscoveryGroup(String name, String binding) {
-        clickButton("Add");
-        getWindowFragment().getEditor().text("name", name);
-        getWindowFragment().getEditor().text("socket-binding", binding);
-        getWindowFragment().clickButton("Save");
+    public AddDiscoveryGroupWizard addDiscoveryGroup() {
+        return getResourceManager().addResource(AddDiscoveryGroupWizard.class);
     }
 
     public void addInVmAcceptor(String name, String server) {
