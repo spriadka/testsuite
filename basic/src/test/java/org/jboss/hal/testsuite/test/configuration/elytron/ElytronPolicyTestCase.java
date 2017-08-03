cbafbdedbf5cc73fa927bbb97a835dd25986992f
Simon Priadka <spriadka@github.com>
2017-08-03 10:29:34 +0200

    Enabled Elytron Policy TestCase and added tests

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/elytron/ElytronPolicyTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/elytron/ElytronPolicyTestCase.java
@@ -1,5 +1,7 @@
 package org.jboss.hal.testsuite.test.configuration.elytron;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.dmr.ModelNode;
@@ -7,9 +9,12 @@ import org.jboss.hal.testsuite.creaper.ResourceVerifier;
 import org.jboss.hal.testsuite.dmr.ModelNodeGenerator;
 import org.jboss.hal.testsuite.fragment.config.AddResourceWizard;
 import org.jboss.hal.testsuite.page.config.elytron.ElytronOtherOtherPage;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.extras.creaper.commands.foundation.online.SnapshotBackup;
+import org.wildfly.extras.creaper.core.CommandFailedException;
 import org.wildfly.extras.creaper.core.online.operations.Address;
 import org.wildfly.extras.creaper.core.online.operations.Values;
 
@@ -18,25 +23,47 @@ import java.util.concurrent.TimeoutException;
 
 import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-@Ignore("Ignored until https://issues.jboss.org/browse/WFLY-9025 is resolved")
 @RunWith(Arquillian.class)
+@RunAsClient
 public class ElytronPolicyTestCase extends AbstractElytronTestCase {
 
     private static final String
             JACC_POLICY = "jacc-policy",
             CUSTOM_POLICY = "custom-policy",
             CUSTOM_POLICY_LABEL = "Custom Policy",
+            JACC_POLICY_LABEL = "JACC Policy",
+            CONFIGURATION_FACTORY = "configuration-factory",
             CLASS_NAME = "class-name",
+            MODULE = "module",
+            NAME = "name",
             DEFAULT_POLICY = "default-policy",
             JACC = "jacc",
             POLICY = "policy",
             POLICY_LABEL = "Policy",
             LIST_ADD = "list-add";
 
+    private static final SnapshotBackup snapshotBackup = new SnapshotBackup();
+
     @Page
     private ElytronOtherOtherPage page;
+
+    @BeforeClass
+    public static void setUp() throws CommandFailedException {
+        client.apply(snapshotBackup.backup());
+    }
+
+    @AfterClass
+    public static void tearDown() throws InterruptedException, TimeoutException, IOException, CommandFailedException {
+        try {
+            client.apply(snapshotBackup.restore());
+        } finally {
+            adminOps.reloadIfRequired();
+        }
+    }
+
 
     /**
      * @tpTestDetails Try to create Policy instance in Web Console's Elytron subsystem configuration.
@@ -58,7 +85,7 @@ public class ElytronPolicyTestCase extends AbstractElytronTestCase {
                     .saveAndDismissReloadRequiredWindowWithState()
                     .assertWindowClosed();
 
-            assertTrue("Probably fails because of https://issues.jboss.org/browse/WFLY-9015",
+            assertTrue("Policy should be present in the table",
                     page.getResourceManager().isResourcePresent(policyAddress.getLastPairValue()));
 
             new ResourceVerifier(policyAddress, client).verifyExists();
@@ -67,6 +94,42 @@ public class ElytronPolicyTestCase extends AbstractElytronTestCase {
             adminOps.reloadIfRequired();
         }
     }
+
+    /**
+     * @tpTestDetails Try to create Elytron Policy instance in Web Console's configuration without required name field
+     * Validate that a validation message is shown in form
+     */
+    @Test
+    public void testAddPolicyWithoutNameShowsError() throws Exception {
+        page.navigateToApplication().switchSubTab(POLICY_LABEL);
+        page.getResourceManager()
+                .addResource(AddResourceWizard.class)
+                .saveAndDismissReloadRequiredWindowWithState().assertWindowOpen();
+        assertTrue("An validation error regarding missing policy name should be visible"
+                , page.getWindowFragment().isErrorShownInForm());
+    }
+
+    /**
+     * @tpTestDetails Create Elytron Policy instance in model and try to create Elytron Policy instance with same name in Web Console's configuration
+     * Validate that a validation message in alert area is shown
+     */
+    @Test
+    public void testAddPolicyDuplicateShowsError() throws Exception {
+        final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
+        try {
+            createPolicy(policyAddress);
+            page.navigateToApplication().switchSubTab(POLICY_LABEL);
+            page.getResourceManager()
+                    .addResource(AddResourceWizard.class)
+                    .name(JACC)
+                    .saveAndDismissReloadRequiredWindowWithState().assertWindowClosed();
+            assertNotNull("There should be warning regarding duplicate resource", page.getAlertArea());
+        } finally {
+            ops.removeIfExists(policyAddress);
+            adminOps.reloadIfRequired();
+        }
+    }
+
 
     /**
      * @tpTestDetails Create Elytron Policy instance in model and try to remove it in Web Console's Elytron subsystem
@@ -99,12 +162,12 @@ public class ElytronPolicyTestCase extends AbstractElytronTestCase {
 
     /**
      * @tpTestDetails Create Elytron Policy instance in model and try to add its custom-policy child in Web Console's
-     * configuration.
+     * configuration, filling only required fields
      * Validate the resource is visible in Elytron Policy's custom-policy table.
      * Validate created resource is present in the model.
      */
     @Test
-    public void testAddCustomPolicy() throws Exception {
+    public void testAddCustomPolicyRequiredFieldOnly() throws Exception {
         final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
         final String customPolicyName = randomAlphabetic(7),
                 classNameValue = randomAlphabetic(7);
@@ -130,10 +193,92 @@ public class ElytronPolicyTestCase extends AbstractElytronTestCase {
 
             new ResourceVerifier(policyAddress, client)
                     .verifyListAttributeContainsValue(CUSTOM_POLICY, new ModelNodeGenerator.ModelNodePropertiesBuilder()
-                                    .addProperty(NAME, customPolicyName)
-                                    .addProperty(CLASS_NAME, classNameValue)
-                                    .addUndefinedProperty(MODULE)
-                                    .build());
+                            .addProperty(NAME, customPolicyName)
+                            .addProperty(CLASS_NAME, classNameValue)
+                            .addUndefinedProperty(MODULE)
+                            .build());
+        } finally {
+            ops.removeIfExists(policyAddress);
+            adminOps.reloadIfRequired();
+
+        }
+    }
+
+    /**
+     * @tpTestDetails Create Elytron Policy instance in model and try to add its custom-policy child in Web Console's
+     * configuration, filling required and optional fields.
+     * Validate the resource is visible in Elytron Policy's custom-policy table.
+     * Validate created resource is present in the model.
+     */
+    @Test
+    public void testAddCustomPolicyWithOptionalFields() throws Exception {
+        final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
+        final String customPolicyName = randomAlphabetic(7),
+                classNameValue = randomAlphabetic(7),
+                moduleValue = "my_module_" + RandomStringUtils.randomAlphanumeric(7);
+        try {
+            createPolicy(policyAddress);
+            page.navigateToApplication()
+                    .switchSubTab(POLICY_LABEL);
+            AddResourceWizard wizard = page.getConfig()
+                    .switchTo(CUSTOM_POLICY_LABEL)
+                    .getResourceManager()
+                    .addResource(AddResourceWizard.class);
+            wizard
+                    .name(customPolicyName)
+                    .text(CLASS_NAME, classNameValue);
+            wizard.openOptionalFieldsTab();
+            wizard.text(MODULE, moduleValue)
+                    .saveAndDismissReloadRequiredWindowWithState()
+                    .assertWindowClosed();
+            assertTrue(page.getConfig()
+                    .switchTo(CUSTOM_POLICY_LABEL)
+                    .getResourceManager()
+                    .isResourcePresent(customPolicyName));
+            new ResourceVerifier(policyAddress, client)
+                    .verifyListAttributeContainsValue(CUSTOM_POLICY, new ModelNodeGenerator.ModelNodePropertiesBuilder()
+                            .addProperty(NAME, customPolicyName)
+                            .addProperty(CLASS_NAME, classNameValue)
+                            .addProperty(MODULE, moduleValue)
+                            .build());
+        } finally {
+            ops.removeIfExists(policyAddress);
+            adminOps.reloadIfRequired();
+
+        }
+    }
+
+    /**
+     * @tpTestDetails Create Elytron Policy instance in model and try to add its custom-policy child in Web Console without filling required fields
+     * Validate that a validation message is shown in form
+     */
+    @Test
+    public void testAddCustomPolicyWithoutRequiredFieldsShowsError() throws Exception {
+        final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
+        final String customPolicyName = randomAlphabetic(7),
+                classNameValue = randomAlphabetic(7);
+        try {
+            createPolicy(policyAddress);
+            page.navigateToApplication()
+                    .switchSubTab(POLICY_LABEL);
+
+            AddResourceWizard wizard = page.getConfig()
+                    .switchTo(CUSTOM_POLICY_LABEL)
+                    .getResourceManager()
+                    .addResource(AddResourceWizard.class);
+            wizard.saveAndDismissReloadRequiredWindowWithState().assertWindowOpen();
+            assertTrue("Validation errors regarding missing policy name and class should be visible",
+                    page.getWindowFragment().isErrorShownInForm());
+            wizard
+                    .name(customPolicyName)
+                    .saveAndDismissReloadRequiredWindowWithState().assertWindowOpen();
+            assertTrue("Validation error regarding missing policy class should be visible", page.getWindowFragment().isErrorShownInForm());
+            wizard.getEditor().getText(NAME).clear();
+            wizard
+                    .text(CLASS_NAME, classNameValue)
+                    .saveAndDismissReloadRequiredWindowWithState()
+                    .assertWindowOpen();
+            assertTrue("Validation error regarding missing policy name should be visible", page.getWindowFragment().isErrorShownInForm());
         } finally {
             ops.removeIfExists(policyAddress);
             adminOps.reloadIfRequired();
@@ -191,6 +336,148 @@ public class ElytronPolicyTestCase extends AbstractElytronTestCase {
         }
     }
 
+    /**
+     * @tpTestDetails Create Elytron Policy instance in model and try to add its JACC-policy child in Web Console's
+     * configuration, filling only required fields
+     * Validate the resource is visible in Elytron Policy's JACC-policy table.
+     * Validate created resource is present in the model.
+     */
+    @Test
+    public void testAddJACCPolicyRequiredFieldOnly() throws Exception {
+        final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
+        final String jaccPolicyName = randomAlphabetic(7);
+        try {
+            createPolicy(policyAddress);
+
+            page.navigateToApplication()
+                    .switchSubTab(POLICY_LABEL);
+
+            page.getConfig()
+                    .switchTo(JACC_POLICY_LABEL)
+                    .getResourceManager()
+                    .addResource(AddResourceWizard.class)
+                    .name(jaccPolicyName)
+                    .saveAndDismissReloadRequiredWindowWithState()
+                    .assertWindowClosed();
+
+            assertTrue(page.getConfig()
+                    .switchTo(JACC_POLICY_LABEL)
+                    .getResourceManager()
+                    .isResourcePresent(jaccPolicyName));
+
+            new ResourceVerifier(policyAddress, client)
+                    .verifyListAttributeContainsValue(JACC_POLICY, new ModelNodeGenerator.ModelNodePropertiesBuilder()
+                            .addProperty(NAME, jaccPolicyName)
+                            .addUndefinedProperty(POLICY)
+                            .addUndefinedProperty(CONFIGURATION_FACTORY)
+                            .addUndefinedProperty(MODULE)
+                            .build());
+        } finally {
+            ops.removeIfExists(policyAddress);
+            adminOps.reloadIfRequired();
+        }
+    }
+
+    /**
+     * @tpTestDetails Create Elytron Policy instance in model and try to add its JACC-policy child in Web Console's
+     * configuration, filling required and optional fields.
+     * Validate the resource is visible in Elytron Policy's JACC-policy table.
+     * Validate created resource is present in the model.
+     */
+    @Test
+    public void testAddJACCPolicyWithOptionalFields() throws Exception {
+        final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
+        final String jaccPolicyName = randomAlphabetic(7);
+        final String jaccPolicyPolicy = "policy_" + RandomStringUtils.randomAlphanumeric(7);
+        final String jaccPolicyConfigurationFactory = "configuration_factory_" + RandomStringUtils.randomAlphanumeric(7);
+        final String jaccPolicyModule = "module_" + RandomStringUtils.randomAlphanumeric(7);
+        try {
+            createPolicy(policyAddress);
+
+            page.navigateToApplication()
+                    .switchSubTab(POLICY_LABEL);
+
+            page.getConfig()
+                    .switchTo(JACC_POLICY_LABEL)
+                    .getResourceManager()
+                    .addResource(AddResourceWizard.class)
+                    .name(jaccPolicyName)
+                    .text(POLICY, jaccPolicyPolicy)
+                    .text(CONFIGURATION_FACTORY, jaccPolicyConfigurationFactory)
+                    .text(MODULE, jaccPolicyModule)
+                    .saveAndDismissReloadRequiredWindowWithState()
+                    .assertWindowClosed();
+            assertTrue(page.getConfig()
+                    .switchTo(JACC_POLICY_LABEL)
+                    .getResourceManager()
+                    .isResourcePresent(jaccPolicyName));
+
+            new ResourceVerifier(policyAddress, client)
+                    .verifyListAttributeContainsValue(JACC_POLICY, new ModelNodeGenerator.ModelNodePropertiesBuilder()
+                            .addProperty(NAME, jaccPolicyName)
+                            .addProperty(POLICY, jaccPolicyPolicy)
+                            .addProperty(CONFIGURATION_FACTORY, jaccPolicyConfigurationFactory)
+                            .addProperty(MODULE, jaccPolicyModule)
+                            .build());
+        } finally {
+            ops.removeIfExists(policyAddress);
+            adminOps.reloadIfRequired();
+        }
+    }
+
+    /**
+     * @tpTestDetails Create Elytron Policy instance in model and then add its JACC-policy child in model. Then try to
+     * remove it in Web Console's configuration.
+     * Validate the resource is not visible in Elytron Policy's JACC-policy table anymore.
+     * Validate created resource is not present in the model.
+     */
+    @Test
+    public void testRemoveJACCPolicy() throws Exception {
+        final Address policyAddress = elyOps.getElytronAddress(POLICY, JACC);
+        final String jaccPolicyName = randomAlphabetic(7);
+
+        final ModelNode customPolicyModelNode = new ModelNodeGenerator.ModelNodePropertiesBuilder()
+                .addProperty(NAME, jaccPolicyName)
+                .addUndefinedProperty(POLICY)
+                .addUndefinedProperty(CONFIGURATION_FACTORY)
+                .addUndefinedProperty(MODULE)
+                .build();
+        try {
+            createPolicy(policyAddress);
+            ops.invoke(LIST_ADD, policyAddress, Values.empty()
+                    .and(NAME, JACC_POLICY)
+                    .and("value", customPolicyModelNode)).assertSuccess();
+            adminOps.reloadIfRequired();
+
+            page.navigateToApplication()
+                    .switchSubTab(POLICY_LABEL);
+
+            page.getConfig()
+                    .switchTo(JACC_POLICY_LABEL)
+                    .getResourceManager()
+                    .removeResource(jaccPolicyName)
+                    .confirmAndDismissReloadRequiredMessage()
+                    .assertClosed();
+
+            assertFalse(page.getConfig()
+                    .switchTo(JACC_POLICY_LABEL)
+                    .getResourceManager()
+                    .isResourcePresent(jaccPolicyName));
+
+            new ResourceVerifier(policyAddress, client)
+                    .verifyListAttributeDoesNotContainValue(JACC_POLICY, new ModelNodeGenerator.ModelNodePropertiesBuilder()
+                            .addProperty(NAME, jaccPolicyName)
+                            .addUndefinedProperty(MODULE)
+                            .addUndefinedProperty(POLICY)
+                            .addUndefinedProperty(CONFIGURATION_FACTORY)
+                            .addUndefinedProperty(MODULE)
+                            .build());
+        } finally {
+            ops.removeIfExists(policyAddress);
+            adminOps.reloadIfRequired();
+        }
+    }
+
     private void createPolicy(Address address) throws IOException, TimeoutException, InterruptedException {
         ops.add(address, Values.of(JACC_POLICY, new ModelNodeGenerator.ModelNodeListBuilder(
                 new ModelNodeGenerator.ModelNodePropertiesBuilder()
@@ -198,6 +485,8 @@ public class ElytronPolicyTestCase extends AbstractElytronTestCase {
                         .build()
         ).build())).assertSuccess();
         adminOps.reloadIfRequired();
+        //TODO: remove when JBEAP-12434 is fixed
+        adminOps.restart();
     }
 
 }
