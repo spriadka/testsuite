55ba4da5ad91c870db9fb24269c8e04124147997
Simon Priadka <spriadka@github.com>
2017-08-03 10:29:34 +0200

    Enabled Elytron Policy TestCase and added tests

--- common/src/main/java/org/jboss/hal/testsuite/page/config/elytron/ElytronOtherOtherPage.java
+++ common/src/main/java/org/jboss/hal/testsuite/page/config/elytron/ElytronOtherOtherPage.java
@@ -1,5 +1,10 @@
 package org.jboss.hal.testsuite.page.config.elytron;
 
+import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.hal.testsuite.fragment.ConfigFragment;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static org.jboss.hal.testsuite.finder.FinderNames.SETTINGS;
 import static org.jboss.hal.testsuite.page.config.elytron.ElytronPageConstants.ELYTRON_SUBSYTEM_LABEL;
 
@@ -10,5 +15,10 @@ public class ElytronOtherOtherPage extends AbstractElytronConfigPage<ElytronOthe
         getSubsystemNavigation(ELYTRON_SUBSYTEM_LABEL).step(SETTINGS, "Other").openApplication();
         switchTab("Other");
         return this;
+    }
+
+    public ConfigFragment getWindowFragment() {
+        WebElement editPanel = browser.findElement(By.className("default-window-content"));
+        return Graphene.createPageFragment(ConfigFragment.class, editPanel);
     }
 }
