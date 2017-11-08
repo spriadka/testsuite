e1436c140dd13906c357c7640d5b8a854a070199
Simon Priadka <spriadka@github.com>
2017-11-08 16:08:15 +0100

    Added AggregateProvidersTestCase and AddAggregateProvidersWizard

--- basic/src/test/java/org/jboss/hal/testsuite/test/configuration/elytron/other/ssl/AggregateProvidersTestCase.java
+++ basic/src/test/java/org/jboss/hal/testsuite/test/configuration/elytron/other/ssl/AggregateProvidersTestCase.java
@@ -1,19 +1,20 @@
-package org.jboss.hal.testsuite.test.configuration.elytron;
+package org.jboss.hal.testsuite.test.configuration.elytron.other.ssl;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.jboss.hal.testsuite.util.ConfigChecker.InputType.TEXT;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.dmr.ModelNode;
 import org.jboss.hal.testsuite.category.Elytron;
 import org.jboss.hal.testsuite.creaper.ResourceVerifier;
 import org.jboss.hal.testsuite.dmr.ModelNodeGenerator.ModelNodeListBuilder;
-import org.jboss.hal.testsuite.fragment.formeditor.Editor;
-import org.jboss.hal.testsuite.fragment.shared.modal.WizardWindow;
+import org.jboss.hal.testsuite.fragment.config.elytron.other.ssl.AddAggregateProvidersWizard;
 import org.jboss.hal.testsuite.page.config.elytron.SSLPage;
+import org.jboss.hal.testsuite.test.configuration.elytron.AbstractElytronTestCase;
 import org.jboss.hal.testsuite.util.ConfigChecker;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -22,14 +23,14 @@ import org.wildfly.extras.creaper.core.online.operations.Address;
 import org.wildfly.extras.creaper.core.online.operations.Values;
 
 @RunWith(Arquillian.class)
+@RunAsClient
 @Category(Elytron.class)
-public class ElytronProviderTestCase extends AbstractElytronTestCase {
+public class AggregateProvidersTestCase extends AbstractElytronTestCase {
 
-    private static final String
-        PROVIDER_LOADER = "provider-loader",
-        AGGREGATE_PROVIDERS = "aggregate-providers",
-        AGGREGATE_PROVIDERS_LABEL = "Aggregate Providers",
-        PROVIDERS = "providers";
+    private static final String PROVIDER_LOADER = "provider-loader";
+    private static final String AGGREGATE_PROVIDERS = "aggregate-providers";
+    private static final String AGGREGATE_PROVIDERS_LABEL = "Aggregate Providers";
+    private static final String PROVIDERS = "providers";
 
     @Page
     private SSLPage page;
@@ -43,27 +44,24 @@ public class ElytronProviderTestCase extends AbstractElytronTestCase {
      */
     @Test
     public void addAggregateProvidersTest() throws Exception {
-        String
-            provider1name = randomAlphanumeric(5) + "_1",
-            provider2name = randomAlphanumeric(5) + "_2",
-            aggregateProvidersName = randomAlphanumeric(5);
-        Address
-            provider1address = elyOps.getElytronAddress(PROVIDER_LOADER, provider1name),
-            provider2address = elyOps.getElytronAddress(PROVIDER_LOADER, provider2name),
-            aggregateProvidersAddress = elyOps.getElytronAddress(AGGREGATE_PROVIDERS, aggregateProvidersName);
-
-        page.navigateToApplication().selectResource(AGGREGATE_PROVIDERS_LABEL);
-
+        final String provider1name = randomAlphanumeric(5) + "_1";
+        final String provider2name = randomAlphanumeric(5) + "_2";
+        final String providersValue = String.join("\n", provider1name, provider2name);
+        final String aggregateProvidersName = "aggregate_providers" + randomAlphanumeric(5);
+        final Address provider1address = elyOps.getElytronAddress(PROVIDER_LOADER, provider1name);
+        final Address provider2address = elyOps.getElytronAddress(PROVIDER_LOADER, provider2name);
+        final Address aggregateProvidersAddress = elyOps.getElytronAddress(AGGREGATE_PROVIDERS, aggregateProvidersName);
         try {
             ops.add(provider1address).assertSuccess();
             ops.add(provider2address).assertSuccess();
-
-            WizardWindow wizard = page.getResourceManager().addResource();
-            Editor editor = wizard.getEditor();
-            editor.text(NAME, aggregateProvidersName);
-            editor.text(PROVIDERS, provider1name + "\n" + provider2name);
-
-            assertTrue("Dialog should be closed!", wizard.finish());
+            page.navigateToApplication()
+                    .selectResource(AGGREGATE_PROVIDERS_LABEL)
+                    .getResourceManager()
+                    .addResource(AddAggregateProvidersWizard.class)
+                    .name(aggregateProvidersName)
+                    .providers(providersValue)
+                    .saveAndDismissReloadRequiredWindowWithState()
+                    .assertWindowClosed();
             assertTrue("Created resource should be present in the table!",
                     page.resourceIsPresentInMainTable(aggregateProvidersName));
             new ResourceVerifier(aggregateProvidersAddress, client).verifyExists()
@@ -86,15 +84,12 @@ public class ElytronProviderTestCase extends AbstractElytronTestCase {
      */
     @Test
     public void removeAggregateProvidersTest() throws Exception {
-        String
-            provider1name = randomAlphanumeric(5) + "_1",
-            provider2name = randomAlphanumeric(5) + "_2",
-            aggregateProvidersName = randomAlphanumeric(5);
-        Address
-            provider1address = elyOps.getElytronAddress(PROVIDER_LOADER, provider1name),
-            provider2address = elyOps.getElytronAddress(PROVIDER_LOADER, provider2name),
-            aggregateProvidersAddress = elyOps.getElytronAddress(AGGREGATE_PROVIDERS, aggregateProvidersName);
-
+        final String provider1name = randomAlphanumeric(5) + "_1";
+        final String provider2name = randomAlphanumeric(5) + "_2";
+        final String aggregateProvidersName = "aggregate_providers_" + randomAlphanumeric(5);
+        final Address provider1address = elyOps.getElytronAddress(PROVIDER_LOADER, provider1name);
+        final Address provider2address = elyOps.getElytronAddress(PROVIDER_LOADER, provider2name);
+        final Address aggregateProvidersAddress = elyOps.getElytronAddress(AGGREGATE_PROVIDERS, aggregateProvidersName);
         try {
             ops.add(provider1address).assertSuccess();
             ops.add(provider2address).assertSuccess();
@@ -102,8 +97,12 @@ public class ElytronProviderTestCase extends AbstractElytronTestCase {
                             .addNode(new ModelNode(provider1name))
                             .addNode(new ModelNode(provider2name)).build()));
 
-            page.navigateToApplication().selectResource(AGGREGATE_PROVIDERS_LABEL).getResourceManager()
-                    .removeResource(aggregateProvidersName).confirmAndDismissReloadRequiredMessage().assertClosed();
+            page.navigateToApplication()
+                    .selectResource(AGGREGATE_PROVIDERS_LABEL)
+                    .getResourceManager()
+                    .removeResource(aggregateProvidersName)
+                    .confirmAndDismissReloadRequiredMessage()
+                    .assertClosed();
             assertFalse("Removed resource should not be present in the table any more!",
                     page.resourceIsPresentInMainTable(aggregateProvidersName));
             new ResourceVerifier(aggregateProvidersAddress, client).verifyDoesNotExist();
@@ -122,17 +121,15 @@ public class ElytronProviderTestCase extends AbstractElytronTestCase {
      */
     @Test
     public void editAggregateProvidersAttributesTest() throws Exception {
-        String
-            provider1name = randomAlphanumeric(5) + "_1",
-            provider2name = randomAlphanumeric(5) + "_2",
-            provider3name = randomAlphanumeric(5) + "_3",
-            aggregateProvidersName = randomAlphanumeric(5);
-        Address
-            provider1address = elyOps.getElytronAddress(PROVIDER_LOADER, provider1name),
-            provider2address = elyOps.getElytronAddress(PROVIDER_LOADER, provider2name),
-            provider3address = elyOps.getElytronAddress(PROVIDER_LOADER, provider3name),
-            aggregateProvidersAddress = elyOps.getElytronAddress(AGGREGATE_PROVIDERS, aggregateProvidersName);
-
+        final String provider1name = randomAlphanumeric(5) + "_1";
+        final String provider2name = randomAlphanumeric(5) + "_2";
+        final String provider3name = randomAlphanumeric(5) + "_3";
+        final String providersValue = String.join("\n", provider2name, provider3name);
+        final String aggregateProvidersName = "aggregate_providers_" + randomAlphanumeric(5);
+        final Address provider1address = elyOps.getElytronAddress(PROVIDER_LOADER, provider1name);
+        final Address provider2address = elyOps.getElytronAddress(PROVIDER_LOADER, provider2name);
+        final Address provider3address = elyOps.getElytronAddress(PROVIDER_LOADER, provider3name);
+        final Address aggregateProvidersAddress = elyOps.getElytronAddress(AGGREGATE_PROVIDERS, aggregateProvidersName);
         try {
             ops.add(provider1address).assertSuccess();
             ops.add(provider2address).assertSuccess();
@@ -140,13 +137,15 @@ public class ElytronProviderTestCase extends AbstractElytronTestCase {
             ops.add(aggregateProvidersAddress, Values.of(PROVIDERS, new ModelNodeListBuilder()
                             .addNode(new ModelNode(provider1name))
                             .addNode(new ModelNode(provider2name)).build()));
-
-            page.navigateToApplication().selectResource(AGGREGATE_PROVIDERS_LABEL).getResourceManager()
+            page.navigateToApplication()
+                    .selectResource(AGGREGATE_PROVIDERS_LABEL)
+                    .getResourceManager()
                     .selectByName(aggregateProvidersName);
             page.switchToConfigAreaTab(ATTRIBUTES_LABEL);
-
-            new ConfigChecker.Builder(client, aggregateProvidersAddress).configFragment(page.getConfigFragment())
-                    .editAndSave(TEXT, PROVIDERS, provider2name + "\n" + provider3name).verifyFormSaved()
+            new ConfigChecker.Builder(client, aggregateProvidersAddress)
+                    .configFragment(page.getConfigFragment())
+                    .editAndSave(TEXT, PROVIDERS, providersValue)
+                    .verifyFormSaved()
                     .verifyAttribute(PROVIDERS, new ModelNodeListBuilder()
                             .addNode(new ModelNode(provider2name))
                             .addNode(new ModelNode(provider3name)).build());
